99d94ede41eec1e9c880436292cf6343d2225910
Huytq <tran.quang.huy@mqsolutions.com.vn>
2021-05-17 16:11:03 +0700

    rotating the box

---
+++ Random Topics/Rotating the Box/solution.go
@@ -0,0 +1,31 @@
+/*
+First we let the gravity fall by each row ( mutating the box ), i mark the "ground" as the last "object" we meet.
+Then we create new 2d slice result which each column of it is column of box with reverse index
+*/
+
+func rotateTheBox(box [][]byte) [][]byte {
+    for i := 0; i < len(box); i++ {
+        lastObj := len(box[0])
+        for j := len(box[0]) - 1; j >= 0; j-- {
+            if(box[i][j] == '*') {
+                lastObj = j
+            }
+            if(box[i][j] == '#') {
+                box[i][j] = '.'
+                box[i][lastObj - 1] = '#'
+                lastObj = lastObj - 1
+            }
+        }
+    }
+    result := make([][]byte, len(box[0]));
+    for i := 0; i < len(result); i++ {
+        result[i] = make([]byte, len(box));
+    }
+    for i := 0; i < len(box[0]); i++ {
+        for j := 0; j < len(box); j++ {
+            result[i][j] = box[len(box) - 1 - j][i]
+        }
+    }
+    return result
+}
+
