80ca2caa19aaa70d419445f3ee1044a2bd641416
HuyTQ <ygoldendarkness@gmail.com>
2021-05-30 14:32:48 +0700

    Smallest Subtree With All Deepest Leaf Nodes

--- Tree & Binary Tree/Lowest Common Ancestor of a Binary Tree/first_approach.go
+++ Tree & Binary Tree/Lowest Common Ancestor of a Binary Tree/first_approach.go
@@ -1,57 +1,58 @@
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
- func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-     levelP := getHeight(root, p, 0)
-     levelQ := getHeight(root, q, 0)
-     for levelP > levelQ {
-         p = getParent(root, p)
-         levelP--
-     }
-     for levelQ > levelP {
-         q = getParent(root, q)
-         levelQ--
-     }
-     for p != q {
-         p = getParent(root, p)
-         q = getParent(root, q)
-     }
-     return p
+package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	levelP := getHeight(root, p, 0)
+	levelQ := getHeight(root, q, 0)
+	for levelP > levelQ {
+		p = getParent(root, p)
+		levelP--
+	}
+	for levelQ > levelP {
+		q = getParent(root, q)
+		levelQ--
+	}
+	for p != q {
+		p = getParent(root, p)
+		q = getParent(root, q)
+	}
+	return p
 }
 
 func getHeight(current *TreeNode, target *TreeNode, level int) int {
-    if current == nil {
-        return 0
-    }
-    if current == target {
-        return level
-    }
-    left := getHeight(current.Left, target, level + 1)
-    right := getHeight(current.Right, target, level + 1)
-    if left != 0 {
-        return left
-    } else {
-        return right
-    }
+	if current == nil {
+		return 0
+	}
+	if current == target {
+		return level
+	}
+	left := getHeight(current.Left, target, level+1)
+	right := getHeight(current.Right, target, level+1)
+	if left != 0 {
+		return left
+	} else {
+		return right
+	}
 }
 
 func getParent(current *TreeNode, target *TreeNode) *TreeNode {
-        if current == nil {
-            return nil
-        }
-        if current.Left == target || current.Right == target {
-            return current
-        }
-        left := getParent(current.Left, target)
-        right := getParent(current.Right, target)
-        if left != nil {
-            return left
-        } else {
-            return right
-        }
-    }
+	if current == nil {
+		return nil
+	}
+	if current.Left == target || current.Right == target {
+		return current
+	}
+	left := getParent(current.Left, target)
+	right := getParent(current.Right, target)
+	if left != nil {
+		return left
+	} else {
+		return right
+	}
+}
--- Tree & Binary Tree/Smallest Subtree With All Deepest Leaf Nodes/solution.go
+++ Tree & Binary Tree/Smallest Subtree With All Deepest Leaf Nodes/solution.go
@@ -1,36 +1,37 @@
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
+package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
 func subtreeWithAllDeepest(root *TreeNode) *TreeNode {
-    result, _ := helper(root, 0)
-    return result
+	result, _ := helper(root, 0)
+	return result
 }
 
 func helper(current *TreeNode, level int) (*TreeNode, int) {
-    if current.Left == nil && current.Right == nil {
-        return current, level
-    }
-    if current.Left == nil {
-        return helper(current.Right, level + 1)
-    }
-    if current.Right == nil {
-        return helper(current.Left, level + 1)
-    }
-    
-    left, levelLeft := helper(current.Left, level + 1)
-    right, levelRight := helper(current.Right, level + 1)
-    
-        if levelLeft > levelRight {
-            return left, levelLeft
-        }
-        if levelLeft < levelRight {
-            return right,levelRight
-        }
-        return current, levelLeft
-    
+	if current.Left == nil && current.Right == nil {
+		return current, level
+	}
+	if current.Left == nil {
+		return helper(current.Right, level+1)
+	}
+	if current.Right == nil {
+		return helper(current.Left, level+1)
+	}
+
+	left, levelLeft := helper(current.Left, level+1)
+	right, levelRight := helper(current.Right, level+1)
+
+	if levelLeft > levelRight {
+		return left, levelLeft
+	}
+	if levelLeft < levelRight {
+		return right, levelRight
+	}
+	return current, levelLeft
+
 }
