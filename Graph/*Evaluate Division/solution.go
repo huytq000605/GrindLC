3e11ec498d6d39e8600bb52663cb829a4f142f90
HuyTQ <ygoldendarkness@gmail.com>
2021-06-07 23:54:14 +0700

    Update evaludate Division

--- Graph/*Evaluate Division/solution.go
+++ Graph/*Evaluate Division/solution.go
@@ -5,15 +5,17 @@ type divide map[string]float64
 func calcEquation(equations [][]string, values []float64, queries [][]string) []float64 {
 	equationMap := make(map[string]divide)
 	/* Make a map have structure like
-	a : {
-		b: 2
-		c: 3
-	},
-	b: {
-		a: 0.5
-	}
-	c: {
-		a: 0.33
+	Map {
+		a : Map {
+			b: 2
+			c: 3
+		},
+		b: Map {
+			a: 0.5
+		}
+		c: Map {
+			a: 0.33
+		}
 	}
 	*/
 	for idx, equation := range equations {
