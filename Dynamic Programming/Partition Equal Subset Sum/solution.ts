d8017397e75f81a0c8d855a693be73f8aa14bc64
HuyTQ <ygoldendarkness@gmail.com>
2021-07-28 23:19:21 +0700

    Partition Equal Subset Sum

--- Dynamic Programming/Partition Equal Subset Sum/solution.ts
+++ Dynamic Programming/Partition Equal Subset Sum/solution.ts
@@ -1,22 +1,28 @@
 function canPartition(nums: number[]): boolean {
+    nums.sort((a,b) => a-b)
     let sum = 0
     for(let num of nums) {
         sum += num
     }
-    let half = sum / 2
     let cache = new Map()
-    function helper(index: number, sum: number) {
-        if(index === nums.length) {
-            if(sum === half) return true
-            else return false
-        }
-        if(sum > half) return false
-        const key = `${index},${sum}`
+    function helper(index: number, current: number) {
+        const key = `${index},${current}`
         if(cache.has(key)) return cache.get(key)
-        let res1 = helper(index + 1, sum + nums[index])
-        let res2 = helper(index + 1, sum)
-        cache.set(key, res1 || res2)
-        return cache.get(key)
+        if(current > sum / 2) return false
+        if(current === sum / 2) return true
+        if(index === nums.length) return false
+        if(helper(index + 1, current + nums[index])) {
+            cache.set(key, true)
+            return true
+        }
+    
+        if(helper(index + 1, current)) {
+        cache.set(key, true)
+        return true
+        } 
+   
+        cache.set(key, false)
+        return false
     }
     return helper(0, 0)
 };
