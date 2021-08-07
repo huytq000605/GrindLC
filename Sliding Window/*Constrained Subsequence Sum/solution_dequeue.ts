3070b272e37632540cf4bb1ac3c8712f4d58e486
HuyTQ <ygoldendarkness@gmail.com>
2021-08-07 16:57:11 +0700

    Constrained Subsequence Sum

--- Sliding Window/*Constrained Subsequence Sum/solution_dequeue.ts
+++ Sliding Window/*Constrained Subsequence Sum/solution_dequeue.ts
@@ -1,19 +1,19 @@
+
 function constrainedSubsetSum(nums: number[], k: number): number {
-    let dequeue = []
+    let dequeue = [] // We maintaining the maximum sum from end - k + 1 to end by this dequeue
     let result = Number.MIN_SAFE_INTEGER
     for(let i = 0; i < nums.length; i++) {
-        if(dequeue.length) {
+        if(dequeue.length) { // We only need to check for the first item in dequeue to make sure it contains element out of range
             if(i - dequeue[0][0] > k) {
                 dequeue.shift()
             }
         }
-        let bePushed = 0
+        let bePushed = nums[i]
         if(dequeue.length) {
             bePushed = Math.max(nums[i], nums[i] + dequeue[0][1])
-        } else {
-            bePushed = nums[i]
         }
-        while(dequeue.length && bePushed >= dequeue[dequeue.length - 1][1]) {
+        // Next element to be pushed must be new start or max sum from previous + current, we get the maximum
+        while(dequeue.length && bePushed >= dequeue[dequeue.length - 1][1]) { // We maintain a maximum sum for next element to choose
             dequeue.pop()
         }
         dequeue.push([i, bePushed])
