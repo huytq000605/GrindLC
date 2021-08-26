6ded621eb6a9ab1ce389f8e434a58ef6284cab0e
HuyTQ <ygoldendarkness@gmail.com>
2021-08-26 18:52:13 +0700

    Verify Preorder Serialization of a Binary Tree

--- Tree & Binary Tree/Verify Preorder Serialization of a Binary Tree/solution.ts
+++ Tree & Binary Tree/Verify Preorder Serialization of a Binary Tree/solution.ts
@@ -1,24 +1,20 @@
 function isValidSerialization(preorder: string): boolean {
-    if(preorder === "#") return true
-    let node = preorder.split(",")
+    let arr = preorder.split(",")
     let index = 0
     let dfs = () => {
-        if(index >= node.length || node[index] === "#") return false
-        index++
-        if(index >= node.length) {
-            return false
-        } else if(node[index] !== "#") {
-            if(!dfs()) return false
+        if(index >= arr.length) return false
+        if(arr[index] === "#") {
+            return true
         }
         index++
-        if(index >= node.length) {
-            return false
-        } else if(node[index] !== "#") {
-            if(!dfs()) return false
-        }
+        if(!dfs()) return false
+        index++
+        if(!dfs()) return false
+        
         return true
     }
     if(!dfs()) return false
-    if(index !== node.length - 1) return false
+    if(index !== arr.length - 1) return false
+    
     return true
 };
