790dac19572aa06acdeb4c6697a6de57cb76faca
HuyTQ <ygoldendarkness@gmail.com>
2021-09-10 00:51:20 +0700

    Longest Univalue Path

--- Tree & Binary Tree/Longest Univalue Path/solution.ts
+++ Tree & Binary Tree/Longest Univalue Path/solution.ts
@@ -1,4 +1,3 @@
-
 // Definition for a binary tree node.
 class TreeNode {
     val: number
@@ -12,29 +11,21 @@ class TreeNode {
 }
 
 
- function longestUnivaluePath(node: TreeNode | null): number {
-    if(!node) return 0
-    let result = 1
-    if(node.left && node.left.val === node.val) {
-        result += helper(node.left)
-    } 
-    if(node.right && node.right.val === node.val) {
-        result += helper(node.right)
+ function longestUnivaluePath(root: TreeNode | null): number {
+    let numOfNodes = 1
+    let dfs = (current) => {
+        if(!current) return 0
+        let left = dfs(current.left)
+        let right = dfs(current.right)
+        if(current.left && current.left.val !== current.val) {
+            left = 0
+        }
+        if(current.right && current.right.val !== current.val) {
+            right = 0
+        }
+        numOfNodes = Math.max(numOfNodes, 1 + left + right) // This is starting point so can go both left and right
+        return Math.max(left, right) + 1 // return only left or right
     }
-    let left = longestUnivaluePath(node.left)
-    let right = longestUnivaluePath(node.right)
-    return Math.max(left, right, result - 1) // Each start have two ways to go down, -1 because result is number of nodes
+    dfs(root)
+    return numOfNodes - 1
 };
-
-function helper(node: TreeNode) {
-    let left = 0
-    let right = 0
-    if(node.left && node.left.val === node.val) {
-        left = helper(node.left)
-    }
-    if(node.right && node.right.val === node.val) {
-        right = helper(node.right)
-    }
-    return Math.max(1 + left, 1 + right)
-    
-}
