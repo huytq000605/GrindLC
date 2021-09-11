2e0cd0bcd9819c2ca2a911920d9113bce2eaaa33
HuyTQ <ygoldendarkness@gmail.com>
2021-09-11 18:16:56 +0700

    LRU Cache

--- Design/*LRU Cache/solution.ts
+++ Design/*LRU Cache/solution.ts
@@ -1,91 +1,60 @@
-class LLNode {
-    prev: LLNode;
-    next: LLNode;
-    val: number;
-    key: number;
-    constructor(key: number, val: number) {
-        this.key = key;
-        this.val = val;
-    }
-}
-
 class LRUCache {
-    head: LLNode;
-    tail: LLNode;
-    capacity: number;
-    length: number;
-    map: Map<number, LLNode>;
+    head: DLLNode
+    tail: DLLNode
+    map: Map<number, DLLNode>
+    capacity: number
     constructor(capacity: number) {
-        this.capacity = capacity;
-        this.length = 0;
-        this.head = null;
-        this.tail = null;
-        this.map = new Map();
+        this.capacity = capacity
+        this.head = new DLLNode(null, 0)
+        this.tail = new DLLNode(null, 0)
+        this.head.next = this.tail
+        this.tail.prev = this.head
+        this.map = new Map()
     }
 
     get(key: number): number {
-        if (!this.map.has(key)) {
-            return -1;
+        if(this.map.has(key)) {
+            let value = this.map.get(key).val
+            this.deleteNode(this.map.get(key))
+            this.put(key, value)
+            return value
+        } else {
+            return -1
         }
-        let node = this.map.get(key);
-        let result = node.val;
-        if (this.tail === node) {
-            return result;
-        }
-        if (this.head === node && node.next) {
-            this.head = node.next;
-        }
-        if (node.prev) {
-            node.prev.next = node.next;
-        }
-        if (node.next) {
-            node.next.prev = node.prev;
-        }
-        this.tail.next = node;
-        node.prev = this.tail;
-        this.tail = node;
-        return result;
     }
 
     put(key: number, value: number): void {
-        let node = null;
-        if (this.map.has(key)) {
-            node = this.map.get(key);
-            if (this.head === node && node.next) {
-                this.head = node.next;
-            }
-            node.val = value;
-            if (this.tail === node) {
-                return;
-            }
-            if (node.prev) {
-                node.prev.next = node.next;
-            }
-            if (node.next) {
-                node.next.prev = node.prev;
-            }
-            this.tail.next = node;
-            node.prev = this.tail;
-            this.tail = node;
-        } else {
-            if (this.length === this.capacity) {
-                this.map.delete(this.head.key);
-                this.head = this.head.next;
-                if (this.head) this.head.prev = null;
-                this.length--;
-            }
-            node = new LLNode(key, value);
-            if (this.length === 0) {
-                this.head = node;
-                this.tail = node;
-            } else {
-                this.tail.next = node;
-                node.prev = this.tail;
-                this.tail = node;
-            }
-            if (this.length !== this.capacity) this.length++;
-            this.map.set(key, node);
+        if(this.map.has(key)) this.deleteNode(this.map.get(key))
+        else if(this.map.size === this.capacity) {
+            this.deleteNode(this.head.next)
         }
+        let tail = this.tail.prev
+        let newNode = new DLLNode(key, value)
+        this.map.set(key, newNode)
+        tail.next = newNode
+        newNode.prev = tail
+        newNode.next = this.tail
+        this.tail.prev = newNode
+    }
+
+    private deleteNode(node) {
+        this.map.delete(node.key)
+        let next = node.next
+        let prev = node.prev
+        if(next) next.prev = prev
+        if(prev) prev.next = next
+    }
+}
+
+class DLLNode {
+    key: number
+    val: number
+    prev: DLLNode
+    next: DLLNode
+    
+    constructor(key: number, val: number) {
+        this.key = key
+        this.val = val
     }
 }
 
