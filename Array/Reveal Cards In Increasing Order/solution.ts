cd89d16d2e344e9ecca210fa0f792b9349883cdd
HuyTQ <ygoldendarkness@gmail.com>
2021-09-19 16:53:22 +0700

    Reveal Cards in Increasing Order

--- Array/Reveal Cards In Increasing Order/solution.ts
+++ Array/Reveal Cards In Increasing Order/solution.ts
@@ -1,20 +1,9 @@
-/*
-We do it the reverse way
-*/
-
 function deckRevealedIncreasing(deck: number[]): number[] {
-    deck.sort((a,b) => a-b);
+    deck.sort((a,b) => a-b)
     let result = []
-    helper(deck, result)
+    while(deck.length) {
+        if(result.length) result.unshift(result.pop())
+        result.unshift(deck.pop())
+    }
     return result
 };
-
-function helper(deck: number[], result: number[]) {
-    if(deck.length) {
-        if(result.length) {
-            result.unshift(result.pop())
-        }
-        result.unshift(deck.pop())
-        return helper(deck, result)
-    }
-}
