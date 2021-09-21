00ef49b58c1bde208a1ad29c7cfd1972611b2b6a
HuyTQ <ygoldendarkness@gmail.com>
2021-09-21 21:44:29 +0700

    Maximum Number of Eeaten Apples

---
+++ Priority Queue/Maximum Number of Eaten Apples/solution.ts
@@ -0,0 +1,105 @@
+function eatenApples(apples: number[], days: number[]): number {
+    let minHeap = new MinHeap<any>((a,b) => {
+        if(a[1] < b[1]) return -1
+        if(a[1] === b[1]) return 0
+        return 1
+    })
+    
+    let day = 0
+    let result = 0
+    
+    while(minHeap.length || day < days.length) {
+        while(minHeap.length && day >= minHeap.peek()[1]) {
+            minHeap.pop()
+        }
+        if(day < days.length) {
+            if(apples[day] > 0)
+                minHeap.push([apples[day], days[day] + day])
+        }
+        
+        if(minHeap.length) {
+            let [remainingApples, rottenDay] = minHeap.pop()
+            remainingApples--
+            result++
+            if(remainingApples > 0) {
+                minHeap.push([remainingApples, rottenDay])
+            }
+        }
+        day++
+    }
+    return result
+    
+};
+
+class MinHeap<T> {
+    private heap: T[];
+    private compare: (a: T, b: T) => 1 | 0 | -1
+    constructor(compareFunction: (a:T, b:T) => 1 | 0 | -1) {
+        this.heap = [];
+        this.compare = compareFunction
+    }
+
+	get length(): number{
+		return this.heap.length
+	}
+	
+    public peek(): T | undefined {
+		if(this.length > 0)
+			return this.heap[0];
+		else 
+			return undefined
+    }
+
+    public push(val: T) {
+        this.heap.push(val);
+        this.bubbleUp();
+    }
+    
+    public pop(): T | undefined {
+        if(this.length === 0) {
+            return undefined
+        } 
+        [this.heap[0], this.heap[this.length - 1]] = [this.heap[this.length - 1],this.heap[0]]
+        const pop = this.heap.pop();
+        this.bubbleDown();
+        return pop;
+    }
+
+    private bubbleUp(): void {
+        let current = this.length - 1;
+        while (current > 0) {
+            let parent = Math.ceil(current / 2) - 1;
+            if (this.compare(this.heap[current], this.heap[parent]) == -1) {
+				[this.heap[current], this.heap[parent]] = [ this.heap[parent], this.heap[current]];
+                current = parent;
+            } else {
+                return;
+            }
+        }
+    }
+
+    private bubbleDown(): void {
+        let current = 0;
+        while (true) {
+            let left = current * 2 + 1;
+            if (left >= this.length) return;
+            let right = left + 1;
+            if (right >= this.length) right = left;
+            let smaller: number;
+            if (this.compare(this.heap[left], this.heap[right]) == -1) {
+                smaller = left;
+            } else {
+                smaller = right;
+            }
+            if (this.compare(this.heap[smaller], this.heap[current]) == -1) {
+                [this.heap[smaller], this.heap[current]] = [
+                    this.heap[current],
+                    this.heap[smaller],
+                ];
+                current = smaller;
+            } else {
+                return;
+            }
+        }
+    }
+}
