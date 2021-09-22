a3c60c5c24ed340810e71c9f4e51e10faa25dc03
HuyTQ <ygoldendarkness@gmail.com>
2021-09-22 20:05:08 +0700

    Maximum Length of a Concatenated String with Unique Characters

--- Backtrack/Maximum Length of a Concatenated String with Unique Characters/solution.ts
+++ Backtrack/Maximum Length of a Concatenated String with Unique Characters/solution.ts
@@ -1,38 +1,51 @@
 function maxLength(arr: string[]): number {
-    return buildString(arr, 0, "", new Set())
+    let strs = []
+    for(let i = 0; i < arr.length; i++) {
+        let valid = true
+        let str = Array(26).fill(0)
+        for(let j = 0; j < str.length; j++) {
+            let idx = arr[i].charCodeAt(j) - "a".charCodeAt(0)
+            str[idx]++
+            if(str[idx] === 2) {
+                valid = false
+                break
+            }
+        }
+        if(valid) strs.push(str)
+    }
+    
+    let result = 0
+    let current = Array(26).fill(0)
+    let dfs = (idx) => {
+        if(idx >= strs.length) return
+        dfs(idx + 1) // Pass this string
+        if(check(current, strs[idx])) { // If can add, try to add
+            for(let i = 0; i < 26; i++) {
+                current[i] += strs[idx][i]
+            }
+            result = Math.max(result, count(current))
+            dfs(idx + 1)
+            for(let i = 0; i < 26; i++) {
+                current[i] -= strs[idx][i]
+            }
+        }
+    }
+    
+    dfs(0)
+    return result
 };
 
-function buildString(arr: string[], index: number, current: string, currentMap: Set<string>) {
-    if(index === arr.length) {
-        return current.length
+function check(str1, str2) {
+    for(let i = 0; i < 26; i++) {
+        if(str1[i] === 1 && str2[i] === 1) return false
     }
-    let result = current.length
-    for(let i = index; i < arr.length; i++) {
-        try {
-            addToSet(currentMap, arr[i])
-        } catch {
-            continue
-        }
-        result = Math.max(result, buildString(arr, i + 1, current + arr[i], currentMap))
-        removeFromSet(currentMap, arr[i])
+    return true
+}
+
+function count(str) {
+    let result = 0
+    for(let l of str) {
+        if(l === 1) result++
     }
     return result
-}
-
-function addToSet(set: Set<string>, str: string) {
-    for(let i = 0; i < str.length; i++) {
-        if(set.has(str[i])) {
-            for(let j = 0; j < i; j++) {
-                set.delete(str[j])
-            }
-            throw new Error("Had this letter")
-        }
-        set.add(str[i])
-    }
-}
-
-function removeFromSet(set: Set<string>, str: string) {
-    for(let i = 0; i < str.length; i++) {
-        set.delete(str[i])
-    }
 }
