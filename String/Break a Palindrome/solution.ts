8b0734fd60daab42c0e374babad4ec3fb3361e1b
HuyTQ <ygoldendarkness@gmail.com>
2021-09-23 21:51:28 +0700

    Break a Palindrome

--- String/Break a Palindrome/solution.ts
+++ String/Break a Palindrome/solution.ts
@@ -1,11 +1,8 @@
 function breakPalindrome(palindrome: string): string {
-    if(palindrome.length === 1) {
-        return ""
-    }
-    for(let i = 0; i < palindrome.length; i++) {
-        if(i === Math.floor(palindrome.length / 2) && palindrome.length % 2 === 1) continue
+    if(palindrome.length === 1) return ""
+    for(let i = 0; i < Math.floor(palindrome.length / 2); i++) {
         if(palindrome[i] !== "a") {
-            return palindrome.slice(0, i) + "a"+ palindrome.slice(i + 1) 
+            return palindrome.slice(0, i) + "a" + palindrome.slice(i + 1)
         }
     }
     return palindrome.slice(0, palindrome.length - 1) + "b"
