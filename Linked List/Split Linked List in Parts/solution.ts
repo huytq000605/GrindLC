fe45830b0afe5588d7a5377791f7167bc06af361
HuyTQ <ygoldendarkness@gmail.com>
2021-09-29 20:26:51 +0700

    Split Linked List in Paris

--- Linked List/Split Linked List in Parts/solution.ts
+++ Linked List/Split Linked List in Parts/solution.ts
@@ -1,4 +1,3 @@
-
 // Definition for singly-linked list.
 class ListNode {
     val: number
@@ -10,47 +9,32 @@ class ListNode {
 }
 
 
- function splitListToParts(head: ListNode | null, k: number): Array<ListNode | null> {
-    let listLength = 0;
-    let originalHeadNode = head;
-    while(head) {
-        listLength++
-        head = head.next
-    } // Find length of the Linked list
-
-    let minEachPart = Math.floor(listLength / k) // num of min nodes each part have
-    let numOfPlusPart = listLength % k // number of starting parts which have minEachPart + 1 nodes
-    let result = []
-
-    let current = originalHeadNode;
-    for(let i = 1; i <= k; i++) {
-        let headNode = current // headNode of this part
-        if (!headNode) { // null part
-            result.push(null);
-            continue;
-        }
-        if(i <= numOfPlusPart) { // if this part have minEachPart + 1 nodes
-            for(let j = 0; j < minEachPart + 1; j++) { // find last node of this part to set next to null
-                if(j == minEachPart) {
-                    let next = current.next
-                    current.next = null
-                    current = next;
-                } else {
-                    current = current.next
-                }
+function splitListToParts(head: ListNode | null, k: number): any {
+    let len = 0
+    let current = head
+    while(current) {
+        len++
+        current = current.next
+    }
+    let each = Math.floor(len / k)
+    let firstN = len - each * k // First N element will have + 1 Linked List node
+    let result = Array(k)
+    for(let i = 0; i < k; i++) {
+        let numOfNode = each
+        if(i + 1 <= firstN) numOfNode++
+        if(numOfNode === 0) result[i] = null
+        for(let j = 0; j < numOfNode; j++) {
+            if(j === 0) {
+                result[i] = head
             }
-        } else { // if this part hafve minEachPart nodes
-            for(let j = 0; j < minEachPart; i++) { // find last node of this part to set next to null
-                if(i == minEachPart - 1) {
-                    let next = current.next
-                    current.next = null
-                    current = next
-                } else {
-                    current = current.next
-                }
+            if(j === numOfNode - 1) {
+                let next = head.next
+                head.next = null
+                head = next
+                continue
             }
+            head = head.next
         }
-        result.push(headNode)
     }
     return result
 };
