aaf3e34663e06f44efe136d3d9daf9c16413fb9f
HuyTQ <ygoldendarkness@gmail.com>
2021-09-30 20:26:58 +0700

    Partition to K Equal Sum Subsets

--- Backtrack/Partition to K Equal Sum Subsets/solution.ts
+++ Backtrack/Partition to K Equal Sum Subsets/solution.ts
@@ -1,37 +1,25 @@
 function canPartitionKSubsets(nums: number[], k: number): boolean {
     let sum = 0
-    for(let num of nums) {
-        sum += num
-    }
+    for(let num of nums) sum += num
     if(sum % k !== 0) return false
-    nums.sort((a,b) => b-a)
     let target = sum / k
-    for(let num of nums) {
-        if(num > target) return false
-    }
-    let indexUsed = Array(nums.length).fill(false)
-    const helper = (current: number, groups: number) => {
-        if(current > target) return false
-        if(current === target) {
-            groups--
-            if(groups === 0) {
-                for(let i = 0; i < indexUsed.length; i++) {
-                   if(indexUsed[i] === false) return false
-                }
-                return true
-            }
-            current = 0
+    let seen = new Set()
+    let dfs = (idx: number, group: number, current: number) => {
+        if(group === 0) {
+            if(seen.size === nums.length) return true
+            return false
         }
-        
-        for(let i = 0; i < nums.length; i++) {
-            
-            if(indexUsed[i]) continue
-            indexUsed[i] = true
-            if(helper(current + nums[i], groups)) return true
-            indexUsed[i] = false
+        if(current === target) {
+            return dfs(0, group - 1, 0)
+        }
+        for(let i = idx; i < nums.length; i++) {
+            if(seen.has(i)) continue
+            if(current + nums[i] > target) continue
+            seen.add(i)
+            if(dfs(i + 1, group, current + nums[i])) return true
+            seen.delete(i)
         }
         return false
     }
-    
-    return helper(0, k)
+    return dfs(0, k, 0)
 };
