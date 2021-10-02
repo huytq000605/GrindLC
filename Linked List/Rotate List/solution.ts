c4deab98ed5fec6ee7fa35d3087c85493adae855
HuyTQ <ygoldendarkness@gmail.com>
2021-10-02 15:57:06 +0700

    Rotate List

--- Linked List/Rotate List/solution.ts
+++ Linked List/Rotate List/solution.ts
@@ -1,25 +1,24 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+// Definition for singly-linked list.
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
 
  function rotateRight(head: ListNode | null, k: number): ListNode | null {
     if(!head) return null
     let length = 0  
     let current = head
-    while(current) {
+    while(current) { // Get length of the LL
         length++
         current = current.next
     }
-    k %= length
-    if(k === 0) return head
+    k %= length // Rotate more than k is just k % length
+    if(k === 0) return head // If k === 0 => not rotate
     let newTail = length - k - 1
     current = head
     while(newTail > 0) {
