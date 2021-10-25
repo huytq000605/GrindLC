ee077e9e486fcc4f2d4250bb17958b0960cf94e5
HuyTQ <ygoldendarkness@gmail.com>
2021-10-25 10:13:07 +0700

    Minimum Area Rectangle

--- Geometric/*Minimum Area Rectangle/solution.ts
+++ Geometric/*Minimum Area Rectangle/solution.ts
@@ -3,7 +3,8 @@ function minAreaRect(points: number[][]): number {
     for(let point of points) {
         let x = point[0]
         let y = point[1]
-        pointMap.set(`${x}-${y}`, true)
+        if(!pointMap.has(x)) pointMap.set(x, new Set())
+        pointMap.get(x).add(y)
     }
     let result = Number.MAX_SAFE_INTEGER
     let hasRectangle = false
@@ -14,11 +15,11 @@ function minAreaRect(points: number[][]): number {
             let y1 = point1[1]
             let x2 = point2[0]
             let y2 = point2[1]
-            if(pointMap.has(`${x1}-${y2}`) && pointMap.has(`${x2}-${y1}`)) {
-                hasRectangle = true
-                let area = Math.abs(x1 - x2) * Math.abs(y1 - y2)
-                if(area < result) result = area
-            }
+            if(!pointMap.has(x1) || !pointMap.get(x1).has(y2)) continue
+            if(!pointMap.has(x2) || !pointMap.get(x2).has(y1)) continue
+            hasRectangle = true
+            let area = Math.abs(x1 - x2) * Math.abs(y1 - y2)
+            if(area < result) result = area
         }
     }
     if(!hasRectangle) return 0
