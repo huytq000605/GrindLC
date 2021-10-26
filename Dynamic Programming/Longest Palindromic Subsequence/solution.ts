fcdc704dffbf5b3d279936be3ffa76cfbcdebda0
HuyTQ <ygoldendarkness@gmail.com>
2021-10-26 19:25:30 +0700

    Longest Palindromic Substring

--- Dynamic Programming/Longest Palindromic Subsequence/solution.ts
+++ Dynamic Programming/Longest Palindromic Subsequence/solution.ts
@@ -1,26 +1,21 @@
 function longestPalindromeSubseq(s: string): number {
-    let map = new Map()
-    return longestPalindrome(s, 0, s.length - 1, map)
-}
-
-function longestPalindrome(s: string, left: number, right: number, map):number {
-    const key = `${left}-${right}`
-    if(map.has(key)) {
-        return map.get(key)
+    let dp = Array(s.length).fill(0).map(() => Array(s.length))
+    let dfs = (left: number, right: number) => {
+        if(left > right) {
+            return 0
+        }
+        if(left === right) {
+            return 1
+        }
+        if(dp[left][right] !== undefined) return dp[left][right]
+        let result
+        if(s[left] === s[right]) {
+            result = dfs(left + 1, right - 1) + 2
+        } else {
+            result = Math.max(dfs(left + 1, right), dfs(left, right - 1))
+        }
+        dp[left][right] = result
+        return result
     }
-    if(left > right) {
-        return 0
-    }
-    if(left == right) {
-        return 1
-    }
-    let plus = 0
-    if(s[left] == s[right]) {
-        map.set(key, 2 + longestPalindrome(s, left + 1, right - 1, map))
-        return map.get(key)
-    }
-    const increaseLeft = longestPalindrome(s, left + 1, right, map)
-    const decreaseRight = longestPalindrome(s, left, right - 1, map)
-    map.set(key, Math.max(increaseLeft, decreaseRight))
-    return map.get(key)
+    return dfs(0, s.length - 1)
 }
