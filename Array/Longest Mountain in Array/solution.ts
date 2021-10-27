7daabcb8c0bf76d4b0225189958206d8b0eb4d98
HuyTQ <ygoldendarkness@gmail.com>
2021-10-27 16:44:34 +0700

    Longest Mountain in Array

--- Array/Longest Mountain in Array/solution.ts
+++ Array/Longest Mountain in Array/solution.ts
@@ -1,29 +1,22 @@
 function longestMountain(arr: number[]): number {
-    let start = 0
+    let up = 0;
+    let down = 0
     let result = 0
-    for(let i = 0; i < arr.length; i++) {
-        if(i + 1 < arr.length && arr[i] >= arr[i+1]) {
-            if(arr[i+1] === arr[i]) { // Equal cannot be moutain
-                start = i + 1
-                continue
+    for(let i = 1; i < arr.length; i++) {
+        if(arr[i-1] === arr[i] || (arr[i-1] > arr[i] && up === 0) ) {
+            up = 0
+            down = 0
+        } else if(arr[i - 1] < arr[i]) {
+            if(down > 0) {
+                down = 0
+                up = 0
             }
-            if(i - start + 1 === 1) { // Dont have any element on the left of top
-                start = i + 1
-            } else {
-                let right = 0 // Count how many elements on the right of top
-                i += 1
-                while(i < arr.length && arr[i] < arr[i-1]) {
-                    right++
-                    i++
-                }
-                if(right > 0) {
-                    result = Math.max(result, i - start)
-                }
-                start = i-1 // new start is the last element on the right
-                i-=2 // At the end of loop, i will + 1 so - 2
-                
-            }
+            up++
+        } else if(arr[i - 1] > arr[i]) {
+            down++
         }
+        if(up > 0 && down > 0 && up + down + 1 > result) result = up + down + 1
+        
     }
     return result
 };
