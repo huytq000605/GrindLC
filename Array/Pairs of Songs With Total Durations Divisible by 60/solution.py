e81f33bfa1fa3ba355d42bf247066f2d33071f80
huytq <ygoldendarkness@gmail.com>
2022-01-02 13:43:43 +0700

    Pairs of Songs With Total Durations Divisible by 60

--- Array/Pairs of Songs With Total Durations Divisible by 60/solution.py
+++ Array/Pairs of Songs With Total Durations Divisible by 60/solution.py
@@ -1,14 +1,12 @@
-from typing import List
-
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
-        modMap = [0] * 60
+        seen = Counter()
         result = 0
         for t in time:
-            modMap[t%60] += 1
-        for i in range(31):
-            if i == 30 or i == 0:
-                result += modMap[i] * (modMap[i] - 1) / 2
+            t %= 60
+            if t == 0:
+                result += seen[0]
             else:
-                result += modMap[i] * modMap[60-i]
-        return int(result)
+                result += seen[60 - t]
+            seen[t] += 1
+        return result
