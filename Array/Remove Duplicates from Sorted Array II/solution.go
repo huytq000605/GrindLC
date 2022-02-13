b6c17fc176aef081b937aad0fd906e7d68f833a3
huytq <ygoldendarkness@gmail.com>
2022-02-13 13:43:25 +0700

    Remove Duplicates from Sorted Array II

---
+++ Array/Remove Duplicates from Sorted Array II/solution.go
@@ -0,0 +1,14 @@
+func removeDuplicates(nums []int) int {
+    if len(nums) < 2 {
+        return len(nums)
+    }
+    slow, fast := 2, 2
+    for fast < len(nums) {
+        if nums[fast] > nums[slow - 2] {
+            nums[slow] = nums[fast]
+            slow++
+        }
+        fast++
+    }
+    return slow
+}
