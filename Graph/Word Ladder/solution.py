0b046dd041e133f3336d31bc8d7e0b2435402ce5
huytq <ygoldendarkness@gmail.com>
2022-03-01 23:39:18 +0700

    Word Ladder

--- Graph/Word Ladder/solution.py
+++ Graph/Word Ladder/solution.py
@@ -1,26 +1,16 @@
-from collections import deque
-from typing import *
-
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        words = set()
-        for word in wordList:
-            words.add(word)
-        
-        if endWord not in wordList:
-            return 0
-        
-        seen = set()
-        queue = deque([[beginWord, 1]])
-        
+        words = set(wordList)
+        queue = deque([(beginWord, 1)])
+        n = len(beginWord)
         while queue:
             word, step = queue.popleft()
-            if word == endWord:
-                return step
-            for i in range(len(word)):
-                for charCode in range(ord('a'), ord('z') + 1):
-                    nextWord = word[:i] + chr(charCode) + word[i + 1:]
-                    if nextWord in words and nextWord not in seen:
-                        seen.add(nextWord)
-                        queue.append([nextWord, step + 1])
+            for i in range(n):
+                for letter in map(chr, [ord('a') + i for i in range(26)]):
+                    next_word = word[:i] + letter + word[i+1:]
+                    if next_word in words:
+                        if next_word == endWord:
+                            return step + 1
+                        words.remove(next_word)
+                        queue.append((next_word, step + 1))
         return 0
