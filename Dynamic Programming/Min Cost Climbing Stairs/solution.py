96ecae39ffdcad11deed96f90a0d41caa591f9ce
huytq <huytq000605@gmail.com>
2022-07-10 10:22:20 +0700

    Min Cost Climbing Stairs

---
+++ Dynamic Programming/Min Cost Climbing Stairs/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        a, b = cost[0], cost[1]
+        for i in range(2, len(cost)):
+            a, b = b, min(a, b) + cost[i]
+        return min(a, b)
