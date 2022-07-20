afb31f7ebf1d2717293527d0e3718c2716c40796
huytq <huytq000605@gmail.com>
2022-07-20 10:40:50 +0700

    Number of Matching Subsequences

--- String/Number of Matching Subsequences/solution.py
+++ String/Number of Matching Subsequences/solution.py
@@ -3,20 +3,22 @@ class Solution:
         result = 0
         d = defaultdict(list)
         for word in words:
-            d[word[0]].append(word)
-        idx = 0
-        while d and idx < len(s):
-            nxt = defaultdict(list)
-            for start_letter in d.keys():
-                if start_letter == s[idx]:
-                    for word in d[start_letter]]:
-                        if len(word) == 1:
-                            result += 1
-                        else:
-                            next_word = word[1:]
-                            nxt[next_word[0]].append(next_word)
-                else:
-                    nxt[start_letter].append([*d[start_letter]])
-            idx += 1
+            d[word[0]].append((word, 0))
+
+        # len(words[i]) <= 50
+        # Time complexity: len(s) + len(S) with S is total length of every word in words 
+        for c in s:
+            for starting in d.keys():
+                if starting == c:
+                    continue
+                nxt[starting] = d[starting]
+
+            if c in d:
+                for word, idx in d[c]:
+                    idx += 1
+                    if idx == len(word):
+                        result += 1
+                    else:
+                        nxt[word[idx]].append((word, idx))
             d = nxt
         return result
