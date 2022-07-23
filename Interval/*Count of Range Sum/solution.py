c9561609fe04a5b8a6a569f7e64b29d8c6e20256
huytq <huytq000605@gmail.com>
2022-07-23 16:37:13 +0700

    Count of Range Sum

--- Interval/*Count of Range Sum/solution.py
+++ Interval/*Count of Range Sum/solution.py
@@ -1,18 +1,25 @@
 class Solution:
     def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
-        n = len(nums)
-        prefix = [0]
-        for i in range(n):
-            prefix.append(prefix[-1] + nums[i])
         result = 0
+        n = len(nums)
+        prefix = [0 for i in range(n)]
+        for i in range(n):
+            if i > 0:
+                prefix[i] = prefix[i-1]
+            prefix[i] += nums[i]
+
         def merge_sort(start, end):
-            nonlocal lower, upper, result
-            if start == end:
+            nonlocal result, prefix, lower, upper
+            if start > end:
+                return []
+            elif start == end:
+                if lower <= prefix[start] <= upper:
+                    result += 1
                 return [prefix[start]]
-            mid = start + (end-start)//2
+            mid = start + (end - start) // 2
             left = merge_sort(start, mid)
-            right = merge_sort(mid + 1, end)
-            
+            right = merge_sort(mid+1, end)
+
             i, j = 0, 0
             for left_prefix in left:
                 while i < len(right) and right[i] - left_prefix < lower:
@@ -20,27 +27,24 @@ class Solution:
                 while j < len(right) and right[j] - left_prefix <= upper:
                     j += 1
                 result += j - i
-                
+
             i, j = 0, 0
             arr = []
-            while i < len(left) or j < len(right):
-                if i >= len(left):
+            while i < len(left) and j < len(right):
+                if left[i] >= right[j]:
                     arr.append(right[j])
                     j += 1
-                    continue
-                
-                if j >= len(right):
-                    arr.append(left[i])
-                    i += 1
-                    continue
-                
-                if left[i] <= right[j]:
-                    arr.append(left[i])
-                    i += 1
                 else:
-                    arr.append(right[j])
-                    j += 1
-                    
+                    arr.append(left[i])
+                    i += 1
+
+            while i < len(left):
+                arr.append(left[i])
+                i += 1
+
+            while j < len(right):
+                arr.append(right[j])
+                j += 1
             return arr
-        merge_sort(0, n)
+        merge_sort(0, n-1)
         return result
