3345de8404165aad490189b7afde5d2684c7d4c5
huytq <huytq000605@gmail.com>
2022-08-11 10:28:43 +0700

    Validate Binary Search Tree

---
+++ Tree & Binary Tree/Validate Binary Search Tree/solution.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        low = -math.inf
+        def dfs(node):
+            nonlocal low
+            if not node:
+                return True
+            if not dfs(node.left) or node.val <= low:
+                return False
+            low = node.val
+            if not dfs(node.right):
+                return False
+            return True
+        return dfs(root)
