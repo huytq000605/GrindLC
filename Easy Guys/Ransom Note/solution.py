c7ca33291299832317eab6389ab97c1e7166118a
huytq <ygoldendarkness@gmail.com>
2022-08-25 16:16:14 +0800

    Ransom Note

---
+++ Easy Guys/Ransom Note/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        counter = Counter(magazine)
+        for letter in ransomNote:
+            if counter[letter] == 0:
+                return False
+            counter[letter] -= 1
+        return True
