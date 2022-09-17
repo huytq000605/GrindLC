b06265b43a382dac320c398f2e745acff3255147
huytq <huytq000605@gmail.com>
2022-09-17 10:56:23 +0800

    Palindrome Pairs

--- String/*Palindrome Pairs/solution.py
+++ String/*Palindrome Pairs/solution.py
@@ -1,38 +1,25 @@
 class Solution:
     def palindromePairs(self, words: List[str]) -> List[List[int]]:
-        wordDict = {}
-        for i in range(len(words)):
-            wordDict[words[i]] = i 
         result = []
+        idxs = {w:i for i, w in enumerate(words)}
         
-        def isPalindrome(s):
-            left = 0
-            right = len(s) - 1
-            while left < right:
-                if s[left] != s[right]: return False
-                left += 1
-                right -= 1
-            return True
+        def is_palindrome(word):
+            return word == word[::-1]
         
-        for i in range(len(words)):
-            word = words[i]
-            for j in range(0, len(word)):
+        for i, word in enumerate(words):
+            for j in range(len(word) + 1):
+                first, second = word[:j], word[j:]
                 
-                first = word[:j]
-                second = word[j:]
-                
-
-                if isPalindrome(first):
-                    revSecond = str(second[::-1])
-                    if revSecond in wordDict and wordDict[revSecond] != i:
-                        result.append([wordDict[revSecond], i])
-                
-                if isPalindrome(second):
-                    revFirst = str(first[::-1])
-                    if revFirst in wordDict and wordDict[revFirst] != i:
-                        result.append([i, wordDict[revFirst]])
-                        if revFirst == "": # Case ["a", ""]
-                            result.append([wordDict[revFirst], i])
+                if is_palindrome(first):
+                    reverse_second = second[::-1]
+                    if reverse_second in idxs and idxs[reverse_second] != i:
+                        result.append((idxs[reverse_second], i))
                         
-                        
+                if is_palindrome(second) and second != "":
+                    reverse_first = first[::-1]
+                    if reverse_first in idxs and idxs[reverse_first] != i:
+                        result.append((i, idxs[reverse_first]))
         return result
+                    
+            
+            
