51f42d8a3736c78589f61122a11ab0a1355f29db
huytq <huytq000605@gmail.com>
2022-09-30 19:51:06 +0800

    Maximum Length of Repeated Subarray

--- Dynamic Programming/Maximum Length of Repeated Subarray/solution.py
+++ Dynamic Programming/Maximum Length of Repeated Subarray/solution.py
@@ -1,24 +1,16 @@
-from typing import List
-from functools import lru_cache
-
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        @lru_cache(None)
-        def helper(start1, start2, starting):
-            if start1 == len(nums1) or start2 == len(nums2):
-                return 0
-            if starting == False:
-                if nums1[start1] != nums2[start2]:
-                    return 0
+        if len(nums1) < len(nums2): 
+            return self.findLength(nums2, nums1)
+        m, n = len(nums1), len(nums2)
+        dp, prevDP = [0] * (n+1), [0] * (n+1)
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if nums1[i] == nums2[j]:
+                    dp[j+1] = prevDP[j] + 1
                 else:
-                    return 1 + helper(start1 + 1, start2 + 1, False)
-            else:
-                res1 = res2 = res3 = 0
-                if nums1[start1] == nums2[start2]:
-                    res1 = 1 + helper(start1 + 1, start2 + 1, False)
-                res2 = helper(start1 + 1, start2, True)
-                res3 = helper(start1, start2 + 1, True)
-                return max(res1, res2, res3)
-        return helper(0, 0, True)
-                    
-            
+                    dp[j+1] = 0
+                ans = max(ans, dp[j+1])
+            dp, prevDP = prevDP, dp
+        return ans
