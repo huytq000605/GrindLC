7da64a6129faad184d29cc14459a81dc04bb3c39
huy.tran <huytq000605@gmail.com>
2022-10-19 19:26:20 +0800

    Top K Frequent Words

--- Priority Queue/Top K Frequent Words/solution.py
+++ Priority Queue/Top K Frequent Words/solution.py
@@ -1,52 +1,22 @@
-from heapq import heappush, heappop
-from typing import List
+class HeapNode:
+    def __init__(self, word, freq):
+        self.word = word
+        self.freq = freq
+        
+    def __lt__(self, other):
+        if self.freq == other.freq:
+            return self.word > other.word
+        return self.freq < other.freq
 
 class Solution:
-    class HeapNode(list):
-        def __lt__(self, other):
-            if self[1] < other[1]:
-                return True
-            elif self[1] == other[1] and self[0] > other[0]:
-                return True
-            else:
-                return False
-            
-        def __gt__(self, other):
-            if self[1] > other[1]:
-                return True
-            elif self[1] == other[1] and self[0] < other[0]:
-                return True
-            else:
-                return False
-
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        freqs = dict()
-        min_heap = []
-        my_list = []
-        for word in words:
-            if(freqs.get(word) == None): 
-                freqs[word] = 0
-            freqs[word] += 1
-            
-        for word, freq in freqs.items():
-            my_list.append(self.HeapNode([word, freq]))
-        
-        
-        for node in reversed(my_list):
-            if len(min_heap) < k:
-                heappush(min_heap, node)
-            else:
-                pop_node = heappop(min_heap)
-                
-                if node < pop_node:
-                    heappush(min_heap, pop_node)
-                else:
-                    heappush(min_heap, node)
+        pq = []
+        counter = Counter(words)
+        for word, freq in counter.items():
+            heappush(pq, HeapNode(word, freq))
+            if len(pq) > k:
+                heappop(pq)
         result = []
-
-        while len(min_heap) > 0:
-            result.insert(0, heappop(min_heap)[0])
-        return result
-    
-        
-                
+        while pq: result.append(heappop(pq).word)
+        return result[::-1]
+            
