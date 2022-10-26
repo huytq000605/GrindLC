1f3f997e57fe4d79d326475b714c808fa5141a8a
huytq <huytq000605@gmail.com>
2022-10-26 19:44:53 +0800

    Continuous Subarray Sum

--- Array/Continuous Subarray Sum/solution.py
+++ Array/Continuous Subarray Sum/solution.py
@@ -1,14 +1,14 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        total = 0
-        prefix = dict()
-        prefix[0] = -1
+        seen = dict()
+        seen[0] = -1
+        cur = 0
         for i, num in enumerate(nums):
-            total += num
-            total %= k
-            if total in prefix:
-                if i - prefix[total] > 1:
+            cur += num
+            cur %= k
+            if cur in seen:
+                if i - seen[cur] > 1:
                     return True
             else:
-                prefix[total] = i
+                seen[cur] = i
         return False
