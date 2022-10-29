df44ba8a97a1b62d23b038cd64efaf068c4ce77b
huytq <huytq000605@gmail.com>
2022-10-29 13:44:03 +0800

    Earliest Possible Dayt of Full Bloom

--- Array/Earliest Possible Day of Full Bloom/solution.py
+++ Array/Earliest Possible Day of Full Bloom/solution.py
@@ -1,14 +1,11 @@
 class Solution:
     def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
         n = len(plantTime)
-        new_plant = []
-        for i in range(n):
-            new_plant.append((growTime[i], plantTime[i]))
-        new_plant.sort(key = lambda plant: (-plant[0], plant[1]))
-        day = 0
+        a = [(plantTime[i], growTime[i]) for i in range(n)]
+        a.sort(key = lambda e: (-e[1], e[0]))
         result = 0
-        for plant in range(n):
-            grow_time, plant_time = new_plant[plant]
-            result = max(result, day + plant_time + grow_time)
-            day += plant_time
+        time = 0
+        for p, g in a:
+            time += p
+            result = max(result, time + g)
         return result
