6139c28c9776ec669b1d2944fcbed27e249ed6bc
huytq <huytq000605@gmail.com>
2022-11-06 19:22:08 +0800

    Apply Operations to an ArraY

---
+++ Easy Guys/Apply Operations to an Array/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for i in range(n-1):
+            if nums[i] == nums[i+1]:
+                nums[i] *= 2
+                nums[i+1] = 0
+        result = []
+        zeros = []
+        for num in nums:
+            if num == 0:
+                zeros.append(num)
+            else:
+                result.append(num)
+        return result + zeros
