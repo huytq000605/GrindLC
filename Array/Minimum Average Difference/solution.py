942ac22114eb3028029d2a8b0bc403068c41c4a5
huy.tran <huytq000605@gmail.com>
2022-12-04 13:36:06 +0700

    Minimum Average Difference

--- Array/Minimum Average Difference/solution.py
+++ Array/Minimum Average Difference/solution.py
@@ -1,22 +1,20 @@
 class Solution:
     def minimumAverageDifference(self, nums: List[int]) -> int:
         n = len(nums)
-        prefix = [0 for i in range(n)]
-        for i, num in enumerate(nums):
-            if i > 0:
-                prefix[i] = prefix[i-1]
-            prefix[i] += num
-            
+        s = sum(nums)
+        prefix = 0
         min_avg_diff = math.inf
         result = 0
-        
         for i in range(n):
-            first = prefix[i] // (i+1)
-            last = 0
-            if i != n-1:
-                last = (prefix[-1] - prefix[i]) // (n-i-1)
-            avg = abs(first - last)
-            if avg < min_avg_diff:
-                min_avg_diff = avg
+            prefix += nums[i]
+            suffix = s - prefix
+            left_avg = prefix // (i+1)
+            if i == n-1:
+                right_avg = 0
+            else:
+                right_avg = suffix // (n-i-1)
+            avg_diff = abs(left_avg - right_avg)
+            if avg_diff < min_avg_diff:
                 result = i
+                min_avg_diff = avg_diff
         return result
