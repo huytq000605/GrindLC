bab84bee4f5b00014348f7fe4a27b218ba62c753
huy.tran <huytq000605@gmail.com>
2022-12-06 15:19:34 +0700

    Odd Even Linked List

--- Linked List/Odd Even Linked List/solution.py
+++ Linked List/Odd Even Linked List/solution.py
@@ -5,30 +5,19 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
-        oddHead = head
-        evenHead = head.next
-        if not evenHead:
-            return head
-        odd = head
-        even = head.next
-        
+        odd, even = ListNode(0), ListNode(0)
+        dummy_odd = odd
+        dummy_even = even
         i = 1
         while head:
-            if i <= 2:
-                head = head.next
-                i += 1
-                continue
             if i % 2 == 1:
                 odd.next = head
                 odd = odd.next
             else:
                 even.next = head
                 even = even.next
-            i += 1
             head = head.next
-        
-        odd.next = evenHead
+            i += 1
+        odd.next = dummy_even.next
         even.next = None
-        return oddHead
+        return dummy_odd.next
