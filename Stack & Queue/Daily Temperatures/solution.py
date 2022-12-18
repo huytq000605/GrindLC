702e125cc60ee14c645addbd0e7a8406e489f693
huy.tran <huytq000605@gmail.com>
2022-12-18 13:36:57 +0700

    Daily Temperatures

--- Stack & Queue/Daily Temperatures/solution.py
+++ Stack & Queue/Daily Temperatures/solution.py
@@ -1,13 +1,12 @@
 from typing import List
 
-
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         stack = []
-        result = [0] * len(temperatures)
+        result = [0 for i in range(len(temperatures))]
         for i, temp in enumerate(temperatures):
-            while len(stack) and temperatures[stack[-1]] < temp:
-                prevIdx = stack.pop()
-                result[prevIdx] = i - prevIdx
+            while stack and temp > temperatures[stack[-1]]:
+                prev_idx = stack.pop()
+                result[prev_idx] = i - prev_idx
             stack.append(i)
         return result
