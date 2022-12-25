ec1f86c0360341fa3cbba575d9ca2bfddb61c852
huy.tran <huytq000605@gmail.com>
2022-12-25 23:05:48 +0700

    Longest Subsequence With Limited Sum

--- Array/Longest Subsequence With Limited Sum/solution.py
+++ Array/Longest Subsequence With Limited Sum/solution.py
@@ -1,14 +1,13 @@
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         nums.sort()
-        result = []
-        for query in queries:
-            total = 0
-            length = 0
-            for num in nums:
-                if total + num > query:
-                    break
-                total += num
-                length += 1
-            result.append(length)
+        queries = sorted([(query, idx) for idx, query in enumerate(queries)])
+        result = [0 for i in range(len(queries))]
+        i = 0
+        s = 0
+        for query, idx in queries:
+            while i < len(nums) and s + nums[i] <= query:
+                s += nums[i]
+                i += 1
+            result[idx] = i
         return result
