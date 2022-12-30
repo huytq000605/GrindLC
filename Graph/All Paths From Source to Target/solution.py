ab7a3a812362ec1bda56c07bce7e1b7fc9ba0d8a
huy.tran <huytq000605@gmail.com>
2022-12-30 11:30:11 +0700

    All Paths From Source to TargeT

--- Graph/All Paths From Source to Target/solution.py
+++ Graph/All Paths From Source to Target/solution.py
@@ -1,14 +1,16 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        path = [0]
         result = []
-        path = []
-        def dfs(node):
-            if node == len(graph) - 1:
-                result.append([*path])
-            for nextNode in graph[node]:
-                path.append(nextNode)
-                dfs(nextNode)
+        n = len(graph)
+        def dfs(u):
+            nonlocal path, result
+            for v in graph[u]:
+                if v == n-1:
+                    result.append([*path, v])
+                    continue
+                path.append(v)
+                dfs(v)
                 path.pop()
-        path.append(0)
         dfs(0)
         return result
