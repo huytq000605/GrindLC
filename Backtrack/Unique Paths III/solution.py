543d854a50ab980e1411b0fe22bce16d0f84cdd7
huy.tran <huytq000605@gmail.com>
2022-12-31 23:44:03 +0700

    Unique Paths III

--- Backtrack/Unique Paths III/solution.py
+++ Backtrack/Unique Paths III/solution.py
@@ -1,43 +1,38 @@
 class Solution:
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
-        dirs = [[0,1], [1,0], [-1,0], [0,-1]]
-        seen = set()
-        m = len(grid)
-        n = len(grid[0])
-        
-        def getId(row, col):
-            return row * n + col
-        
-        start = (0,0)
-        end = (0,0)
+        m, n = len(grid), len(grid[0])
+        seen = [[0 for j in range(n)] for i in range(m)]
+        seen_len = 1
+        ds = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         obstacles = 0
-        result = 0
-        
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    start = (i ,j)
-                elif grid[i][j] == 2:
-                    end = (i ,j)
-                elif grid[i][j] == -1:
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == -1:
                     obstacles += 1
-        seen.add(getId(start[0], start[1]))
+                if grid[r][c] == 1:
+                    start = (r,c)
+                if grid[r][c] == 2:
+                    end = (r,c)
         
+        seen[start[0]][start[1]] = 1
         def dfs(r, c):
-            nonlocal result
+            nonlocal seen_len, seen, ds, obstacles
             if (r, c) == end:
-                if len(seen) + obstacles == m * n:
-                    result += 1
-                return
-            for d in dirs:
-                nr = r + d[0]
-                nc = c + d[1]
-                if nr < 0 or nr >= m or nc < 0 or nc >= n or grid[nr][nc] == -1 or getId(nr, nc) in seen:
+                if seen_len == m * n - obstacles:
+                    return 1
+                return 0
+            result = 0
+            for dr, dc in ds:
+                nr, nc = r + dr, c + dc
+                if nr < 0 or nc < 0 or nr >= m or nc >= n or grid[nr][nc] == -1:
                     continue
-                seen.add(getId(nr, nc))
-                dfs(nr, nc)
-                seen.remove(getId(nr, nc))
-                
-        dfs(start[0], start[1])
+                if seen[nr][nc]:
+                    continue
+                seen[nr][nc] = 1
+                seen_len += 1
+                result += dfs(nr, nc)
+                seen[nr][nc] = 0
+                seen_len -= 1
+            return result
+        return dfs(start[0], start[1])
         
-        return result
