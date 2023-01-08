7120305e0940ae611583e41c26c09f0f22962da9
huytq <huytq000605@gmail.com>
2023-01-08 14:59:55 +0800

    Max Points on a Line

--- Geometric/Max Points on a Line/solution.py
+++ Geometric/Max Points on a Line/solution.py
@@ -1,18 +1,20 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        result = min(2, len(points))
         n = len(points)
+        result = 1
         for i in range(n):
-            for j in range(i + 1, n):
-                point1 = points[i]
-                point2 = points[j]
+            for j in range(i+1, n):
+                x1, y1 = points[i]
+                x2, y2 = points[j]
+                vt = (x2 - x1, y2 - y1)
+                nvt = (vt[1], -vt[0])
+                # equation: ax + by + c = 0
+                # with (a,b) = nvt
+                c = -nvt[0]*x1 - nvt[1] * y1
                 sameLine = 2
-                vt = (point1[0] - point2[0], point1[1] - point2[1]) # vector
-                nvt = (vt[1], -vt[0]) # n vector
-                c = -(nvt[0] * point1[0] + nvt[1] * point1[1]) # ax + by + c = 0 with a is nx and b is ny
                 for k in range(j + 1, n):
-                    point3 = points[k]
-                    if nvt[0] * point3[0] + nvt[1] * point3[1] + c == 0:
+                    x3, y3 = points[k]
+                    if nvt[0]*x3 + nvt[1]*y3 + c == 0:
                         sameLine += 1
-                        result = max(result, sameLine)
+                result = max(result, sameLine)
         return result
