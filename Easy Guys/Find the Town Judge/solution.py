e793845956f315f4c197bb8141ca7c22d509d128
huy.tran <huytq000605@gmail.com>
2023-01-23 13:50:26 +0700

    Find the Town Judge

--- Easy Guys/Find the Town Judge/solution.py
+++ Easy Guys/Find the Town Judge/solution.py
@@ -1,13 +1,12 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        give_trust = [0 for i in range(n)]
-        be_trust  = [0 for i in range(n)]
+        in_deg = [0 for _ in range(n)]
+        out_deg = [0 for _ in range(n)]
         for u, v in trust:
-            u, v = u-1, v-1
-            give_trust[u] += 1
-            be_trust[v] += 1
-    
-        for person in range(n):
-            if give_trust[person] == 0 and be_trust[person] == n - 1:
-                return person + 1
+            out_deg[u-1] += 1
+            in_deg[v-1] += 1
+        for i in range(n):
+            if out_deg[i] == 0 and in_deg[i] == n-1:
+                return i+1
         return -1
+            
