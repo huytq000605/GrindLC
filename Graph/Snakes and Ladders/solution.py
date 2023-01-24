71129f7906a6a5b3817e564c078bd2e77dae43b4
huy.tran <huytq000605@gmail.com>
2023-01-24 15:19:37 +0700

    Snakes and Ladders

--- Graph/Snakes and Ladders/solution.py
+++ Graph/Snakes and Ladders/solution.py
@@ -1,33 +1,33 @@
-from collections import deque
-import math
-from typing import *
-
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         n = len(board)
-        arr = []
-        for i in range(n - 1, -1, -1):
-            if (n - 1 - i) % 2 == 0:
-                for j in range(0, n):
-                    arr.append(board[i][j])
-            else:
-                for j in range(n - 1, -1, -1):
-                    arr.append(board[i][j])
-        queue = deque([[0, 0]])
-        distance = [math.inf] * (n*n)
-        distance[0] = 0
-        while queue:
-            idx, steps = queue.popleft()
-            if idx == n * n - 1:
-                return steps
-            for i in range(1, 7):
-                if idx + i >= n*n:
-                    break
-                if arr[idx + i] != -1 and distance[arr[idx + i] - 1] >= steps + 1:
-                    distance[arr[idx + i] - 1] = steps + 1
-                    queue.append([arr[idx + i] - 1, steps + 1])
-                elif arr[idx+i] == -1 and distance[idx + i] >= steps + 1:
-                    distance[idx + i] = steps + 1
-                    queue.append([idx + i, steps + 1])
-            
+        def get_rc(idx):
+            r = idx // n
+            c = idx % n
+            r = n-1-r
+            if (n-1-r) % 2 == 1:
+                c = n-1-c
+            return r, c
+        
+        def get_idx(r, c):
+            if (n-1-r) % 2 == 1:
+                return (n-1-r) * n + (n-1-c)
+            return (n-1-r) * n + c
+        
+        goal = n*n-1
+        q = deque([(0, 0)])
+        while q:
+            idx, s = q.popleft()
+            r, c = get_rc(idx)
+            if idx == goal:
+                return s
+            for nxt in range(idx + 1, min(idx + 7, n * n)):
+                nr, nc = get_rc(nxt)
+                if board[nr][nc] == -2:
+                    continue
+                if board[nr][nc] != -1:
+                    nxt = board[nr][nc] - 1
+                board[nr][nc] = -2
+                q.append((nxt, s+1))
         return -1
+            
