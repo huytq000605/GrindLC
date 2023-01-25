0bd84b715c5126e73fb620f8efabf07bd69a8ea9
huy.tran <huytq000605@gmail.com>
2023-01-25 13:43:31 +0700

    Find Cloest Node to Given Two Nodes

--- Graph/Find Closest Node to Given Two Nodes/solution.py
+++ Graph/Find Closest Node to Given Two Nodes/solution.py
@@ -1,26 +1,27 @@
 class Solution:
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
         n = len(edges)
-        from_1 = [-1 for i in range(n)]
-        from_2 = [-1 for i in range(n)]
-
-        def traversal(u, arr):
-            arr[u] = 0
-            cur = 0
-            while True:
-                v = edges[u]
-                if v == -1 or arr[v] != -1:
-                    break
-                cur += 1
-                arr[v] = cur
-                u = v
-        traversal(node1, from_1)
-        traversal(node2, from_2)
-
-        result_distance = math.inf
-        result_idx = -1
-        for i in range(n):
-            if from_1[i] != -1 and from_2[i] != -1 and max(from_1[i], from_2[i]) < result_distance:
-                result_idx = i
-                result_distance = max(from_1[i], from_2[i])
-        return result_idx
+        d1 = [-1 for _ in range(n)]
+        d2 = [-1 for _ in range(n)]
+        
+        def dfs(u, d):
+            s = 0
+            d[u] = 0
+            v = edges[u]
+            while v != -1 and d[v] == -1:
+                s += 1
+                d[v] = s
+                v = edges[v]
+        
+        dfs(node1, d1)
+        dfs(node2, d2)
+        
+        d = math.inf
+        result = -1
+        for u in range(n):
+            if d1[u] != -1 and d2[u] != -1:
+                mx = max(d1[u], d2[u])
+                if mx < d:
+                    d = mx
+                    result = u
+        return result
