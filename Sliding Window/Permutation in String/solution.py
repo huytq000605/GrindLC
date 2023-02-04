beaf96d79c782782c6f58700726c430de4171011
huytq <huytq000605@gmail.com>
2023-02-04 13:57:11 +0800

    Permutation in String

--- Sliding Window/Permutation in String/solution.py
+++ Sliding Window/Permutation in String/solution.py
@@ -1,21 +1,21 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        k = len(s1)
-        letter_s1 = [0 for i in range(26)]
-        for l in s1:
-            letter_s1[ord(l) - ord('a')] += 1
-        
-        def compare(arr1, arr2):
-            for i in range(26):
-                if arr1[i] != arr2[i]:
+        count = [0 for i in range(26)]
+        for c in s1:
+            count[ord(c) - ord('a')] += 1
+
+        def is_permutation(count):
+            for freq in count:
+                if freq != 0:
                     return False
             return True
-        
-        letter_s2 = [0 for i in range(26)]
-        for idx, l in enumerate(s2):
-            if idx >= k:
-                letter_s2[ord(s2[idx - k]) - ord('a')] -= 1
-            letter_s2[ord(l) - ord('a')] += 1
-            if idx >= k - 1 and compare(letter_s1, letter_s2):
-                return True
+
+        m = len(s1)
+        for i, c in enumerate(s2):
+            if i >= m:
+                count[ord(s2[i - m]) - ord('a')] += 1
+            count[ord(c) - ord('a')] -= 1
+            if is_permutation(count):
+                return True  
         return False
+                
