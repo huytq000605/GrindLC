33d777f132b80b8b8e476fb8b829737e3e118638
huytq <huytq000605@gmail.com>
2023-02-19 15:03:55 +0800

    Binary Tree Zigzag Level Order Traversal

--- Tree & Binary Tree/Binary Tree Zigzag Level Order Traversal/solution.py
+++ Tree & Binary Tree/Binary Tree Zigzag Level Order Traversal/solution.py
@@ -1,32 +1,31 @@
-from typing import List
-from collections import deque
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        queue = deque([[root, 0]])
-        previousLevel = 0
-        goLeftToRight = True
-        result = [deque([])]
-        while len(queue) > 0:
-            [currentNode, currentLevel] = queue.popleft()
-            if currentLevel != previousLevel:
-                result.append(deque([]))
-                goLeftToRight = not goLeftToRight
-            if goLeftToRight:
-                result[currentLevel].append(currentNode.val)
-            else:
-                result[currentLevel].appendleft(currentNode.val)
-            previousLevel = currentLevel
-            if currentNode.left:
-                queue.append([currentNode.left, currentLevel + 1])
-            if currentNode.right:
-                queue.append([currentNode.right, currentLevel + 1])
+        ltr = True
+        dq = deque([root])
+        result = []
+        while dq:
+            result.append([])
+            ndq = deque()
+            while dq:
+                node = dq.popleft()
+                result[-1].append(node.val)
+                if node.left:
+                    ndq.append(node.left)
+                if node.right:
+                    ndq.append(node.right)
+            if not ltr:
+                result[-1] = result[-1][::-1]
+            ltr = not ltr
+            dq = ndq
         return result
-        
+                    
+            
+
