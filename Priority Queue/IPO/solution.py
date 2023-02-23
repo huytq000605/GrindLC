2fc3757cda7ffa290a22bb2b6817e7e81c3d8984
huytq <huytq000605@gmail.com>
2023-02-23 19:14:06 +0800

    IPO

--- Priority Queue/IPO/solution.py
+++ Priority Queue/IPO/solution.py
@@ -1,15 +1,16 @@
-import heapq
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        projects = [[profits[i], capital[i]] for i in range(len(profits))]
-        projects.sort(key= lambda project: project[1])
-        idx = 0
-        heap = []
-        while k > 0:
-            while idx < len(projects) and projects[idx][1] <= w:
-                heappush(heap, -projects[idx][0])
-                idx += 1
-            if len(heap) == 0: break
-            w += -heappop(heap)
+        n = len(profits)
+        work = sorted([(capital[i], profits[i]) for i in range(n)])
+        can_work = []
+        i = 0
+        while k:
+            while i < n and work[i][0] <= w:
+                c, p = work[i]
+                heappush(can_work, -p)
+                i += 1
+            if not can_work:
+                break
+            w -= heappop(can_work)
             k -= 1
         return w
