9da4ab24758ce70d664c0327a3723030321a7004
huy.tran <huytq000605@gmail.com>
2023-02-24 14:05:47 +0800

    Minize Deviation in Array

--- Priority Queue/Minimize Deviation in Array/solution.py
+++ Priority Queue/Minimize Deviation in Array/solution.py
@@ -1,23 +1,24 @@
 class Solution:
     def minimumDeviation(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return 0
-        heap = []
-        min_num = math.inf
-
-        for num in nums:
-            if num % 2 == 1:
-                num *= 2
-            heappush(heap, -num)
-            min_num = min(min_num, num)
-        
+        n = len(nums)
         result = math.inf
+        min_num = math.inf
+        pq = []
+        # to minize the diff between max & min
+        # need to decrease max or increase min
+        # even number never increase
+        # because the even number can turn back to odd number again
+        # we make the whole array even, then decrease the max
+        for num in nums:
+            if num % 2:
+                num *= 2
+            min_num = min(min_num, num)
+            heappush(pq, -num)
         
-        while -heap[0] % 2 == 0:
-            max_num = -heappop(heap)
+        while -pq[0] % 2 == 0:
+            max_num = -heappop(pq)
             result = min(result, max_num - min_num)
-            heappush(heap, -(max_num // 2))
-            min_num = min(max_num // 2, min_num)
+            min_num = min(min_num, max_num // 2)
+            heappush(pq, -(max_num // 2))
             
-        result = min(result, -heap[0] - min_num)
-        return result
+        return min(result, -pq[0] - min_num)
