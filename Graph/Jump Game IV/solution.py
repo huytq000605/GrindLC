4b7e8f8fe9627e5905d41eb43173be419138a0a5
huytq <huytq000605@gmail.com>
2023-03-05 15:55:17 +0800

    Jump Game IV

--- Graph/Jump Game IV/solution.py
+++ Graph/Jump Game IV/solution.py
@@ -1,36 +1,27 @@
 class Solution:
     def minJumps(self, arr: List[int]) -> int:
-        if len(arr) == 1:
-            return 0
         n = len(arr)
-        same_value = defaultdict(list)
-        
-        for i in range(n):
-            same_value[arr[i]].append(i)
-        
-        seen = set()
-        queue = deque([(0, 0)])
-        seen.add(0)
-        while queue:
-            current_node, step = queue.popleft()
-            if current_node < n-1 and current_node + 1 not in seen:
-                if current_node + 1 == n-1:
-                    return step + 1
-                seen.add(current_node + 1)
-                queue.append((current_node + 1, step + 1))
-                
-            if current_node > 0 and current_node -1 not in seen:
-                seen.add(current_node - 1)
-                queue.append((current_node - 1, step + 1))
-                
-            for next_node in same_value[arr[current_node]]:
-                if next_node in seen:
-                    continue
-                if next_node == n-1:
-                    return step + 1
-                seen.add(next_node)
-                queue.append((next_node, step + 1))
-            
-            same_value[arr[current_node]] = []
-        
+        graph = [[] for _ in range(n)]
+        same = defaultdict(list)
+        for i, num in enumerate(arr):
+            same[num].append(i)
+        seen = [0 for _ in range(n)]
+        seen[0] = 1
+        q = deque([(0, 0)])
+        while q:
+            i, s = q.popleft()
+            if i == n-1: return s
+            if i - 1 >= 0 and not seen[i-1]:
+                seen[i-1] = 1
+                q.append((i-1, s+1))
+            if i + 1 < n and not seen[i+1]:
+                seen[i+1] = 1
+                q.append((i+1, s+1))
+            for ni in same[arr[i]]:
+                if not seen[ni]:
+                    if ni == n-1:
+                        return s+1
+                    seen[ni] = 1
+                    q.append((ni, s+1))
+            same[arr[i]] = []
         return -1
