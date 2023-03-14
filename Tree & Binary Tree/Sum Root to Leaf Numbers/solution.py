099667de76977a076546101cd4cb7b5ad83f36e0
huytq <huytq000605@gmail.com>
2023-03-14 22:05:20 +0800

    Sum Root to Leaf Numbers

--- Tree & Binary Tree/Sum Root to Leaf Numbers/solution.py
+++ Tree & Binary Tree/Sum Root to Leaf Numbers/solution.py
@@ -1,26 +1,17 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def sumNumbers(self, root: TreeNode) -> int:
-        result = 0
-        def getNum(node: TreeNode, current: int):
-            if node == None:
-                return
-            current += node.val
-            if node.left == None and node.right == None:
-                result += current
-                return
-            getNum(node.left, current)
-            getNum(node.right, current)
-        
-        getNum(root, 0)
-        return result
-        
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, cur):
+            if not node: return 0
+            cur = cur * 10 + node.val
+            if not node.left and not node.right:
+                return cur
+            else:
+                return dfs(node.left, cur) + dfs(node.right, cur)
+        return dfs(root, 0)
                 
-        
-        
