453f304e863655ecc5d8f5510321f3573bb5561c
huytq <huytq000605@gmail.com>
2023-03-19 15:06:23 +0800

    Design Add and Search Words Data Structure

--- Design/Design Add and Search Words Data Structure/solution.py
+++ Design/Design Add and Search Words Data Structure/solution.py
@@ -1,38 +1,34 @@
 class WordDictionary:
 
     def __init__(self):
-        self.trie = {}
+        self.trie = dict()
+        
 
     def addWord(self, word: str) -> None:
-        current = self.trie
-        for l in word:
-            if l not in current:
-                current[l] = {}
-            current = current[l]
-        current["word"] = True
-
-    def search_in_trie(self, word, idx, current):
-        if idx == len(word):
-            if "word" in current:
-                return True
-            return False
-        if word[idx] == ".":
-            for key in current.keys():
-                if key == "word": continue
-                if self.search_in_trie(word, idx + 1, current[key]):
-                    return True
-            return False
-        else:
-            if word[idx] not in current:
-                return False
-            return self.search_in_trie(word, idx + 1, current[word[idx]])
+        cur = self.trie
+        for c in word:
+            if c not in cur: cur[c] = dict()
+            cur = cur[c]
+        cur["is_word"] = True
     
+    def dfs(self, word, idx, trie):
+        for i in range(idx, len(word)):
+            c = word[i]
+            if c == ".":
+                for key, next_trie in trie.items():
+                    if key == "is_word": continue
+                    if self.dfs(word, i + 1, next_trie): return True
+                return False
+            
+            if c not in trie: return False
+            trie = trie[c]
+        return "is_word" in trie
+
     def search(self, word: str) -> bool:
-        return self.search_in_trie(word, 0, self.trie)
-                    
+        return self.dfs(word, 0, self.trie)
 
 
-# Your WordDictionary object will beinstantiated and called as such:
+# Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
 # obj.addWord(word)
-# param_2 = obj.search(word) 
+# param_2 = obj.search(word)
