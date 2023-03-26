5e74a443d6768db4970d7293fe5a2f5cc7032f7b
huytq <huytq000605@gmail.com>
2023-03-26 15:56:34 +0800

    Longest cycle in a Graph

--- Graph/Longest Cycle in a Graph/solution.py
+++ Graph/Longest Cycle in a Graph/solution.py
@@ -1,25 +1,22 @@
 class Solution:
     def longestCycle(self, edges: List[int]) -> int:
         n = len(edges)
-        seen = set()
+        seen = [0 for _ in range(n)]
         result = -1
-        # Try to find cycle in each traversal
-        # If we've traversed to a node before, we won't find anything if do it again
+    
         for u in range(n):
-            if u in seen:
-                continue
-            seen.add(u)
+            if seen[u]: continue
             cycle = dict()
-            cycle[u] = 0
             cur = 0
+            cycle[u] = 0
             while True:
                 v = edges[u]
-                cur += 1
                 if v in cycle:
-                    result = max(result, cur - cycle[v])
-                if v == -1 or v in seen:
-                    break
-                seen.add(v)
+                    result = max(result, cur - cycle[v] + 1)
+                if v == -1 or seen[v]: break
+                seen[v] = 1
+                cur += 1
                 cycle[v] = cur
                 u = v
         return result
+            
