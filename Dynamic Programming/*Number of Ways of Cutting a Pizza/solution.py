226cc263dc2940c373d9a161e9b56918160cc696
huytq <huytq000605@gmail.com>
2023-03-31 21:38:08 +0800

    Number of Ways of Cutting a Pizza

--- Dynamic Programming/*Number of Ways of Cutting a Pizza/solution.py
+++ Dynamic Programming/*Number of Ways of Cutting a Pizza/solution.py
@@ -1,36 +1,26 @@
 class Solution:
     def ways(self, pizza: List[str], k: int) -> int:
-        m = len(pizza)
-        n = len(pizza[0])
-        prefix = [[0 for j in range(n + 1)] for i in range(m + 1)]
         MOD = 10**9 + 7
-        
-        for i in range(m - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
-                prefix[i][j] = prefix[i][j + 1] + prefix[i + 1][j] - prefix[i + 1][j + 1]
-                if pizza[i][j] == "A":
-                    prefix[i][j] += 1
-        
+        m, n = len(pizza), len(pizza[0])
+        prefix = [[0 for j in range(n+1)] for i in range(m+1)]
+        for r in reversed(range(m)):
+            for c in reversed(range(n)):
+                val = 1 if pizza[r][c] == "A" else 0
+                prefix[r][c] = prefix[r+1][c] + prefix[r][c+1] - prefix[r+1][c+1] + val
         
         @cache
-        def cut(startRow, startCol, k):
-            if prefix[startRow][startCol] < k:
-                return 0
-            
-            if k == 1:
-                return 1
+        def dfs(r, c, k):
+            if prefix[r][c] < k: return 0
+            if k == 1: return 1
             
             result = 0
-            
-            for row in range(startRow + 1, m):
-                if prefix[startRow][startCol] - prefix[row][startCol] > 0:
-                    result += cut(row, startCol, k - 1)
-                
-            for col in range(startCol + 1, n):
-                if prefix[startRow][startCol] - prefix[startRow][col] > 0:
-                    result += cut(startRow, col, k - 1)
-                
+            # horizontal cut
+            for nr in range(r+1, m):
+                if prefix[r][c] - prefix[nr][c] > 0:
+                    result += dfs(nr,c,k-1)
+            # vertical cut
+            for nc in range(c+1, n):
+                if prefix[r][c] - prefix[r][nc] > 0:
+                    result += dfs(r, nc, k-1)
             return result % MOD
-        
-        return cut(0, 0, k)
-            
+        return dfs(0, 0, k)
