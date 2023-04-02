9b21342d98bd3546df340e42e3b19bcca37ba49a
huytq <huytq000605@gmail.com>
2023-04-02 14:48:21 +0800

    Successful Pairs of Spells and Potions

--- Array/Successful Pairs of Spells and Potions/solution.py
+++ Array/Successful Pairs of Spells and Potions/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-        potions.sort(reverse = True)
         n = len(spells)
-        spells_with_idx = sorted([(spells[i], i) for i in range(n)])
-        result = [0 for i in range(n)]
-        idx = 0
-        for spell, i in spells_with_idx:
-            while idx < len(potions) and potions[idx] * spell >= success:
-                idx += 1
-            result[i] = idx
+        m = len(potions)
+        spells = sorted([(spells[i], i) for i in range(n)])
+        potions.sort(reverse = True)
+        p = 0
+        result = [0 for _ in range(n)]
+        for spell, idx in spells:
+            while p < m and potions[p] * spell >= success:
+                p += 1
+            result[idx] = p
         return result
