a0cac74634f5c2a5507843585d8609089a17194c
huytq <huytq000605@gmail.com>
2023-04-05 00:22:46 +0800

    Optimal Partition of String

--- String/Optimal Partition of String/solution.py
+++ String/Optimal Partition of String/solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        freq = [0 for i in range(26)]
+        mask = 0
         result = 0
         for c in s:
             c = ord(c) - ord('a')
-            if freq[c] > 0:
-                freq = [0 for i in range(26)]
+            if (mask >> c) & 1:
                 result += 1
-            freq[c] += 1
+                mask = 0
+            mask |= (1<<c)
         return result + 1 # last partition
