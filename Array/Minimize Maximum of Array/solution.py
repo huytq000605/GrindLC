a24d6ca30b298b63d3514f1feff66abaf263aef8
huytq <huytq000605@gmail.com>
2023-04-05 23:23:47 +0800

    Minimize Maximum of Array

--- Array/Minimize Maximum of Array/solution.py
+++ Array/Minimize Maximum of Array/solution.py
@@ -1,16 +1,8 @@
 class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
-        result = nums[0]
-        remaining = 0
+        result = 0
+        total = 0
         for i, num in enumerate(nums):
-            if num <= result:
-                remaining += result - num
-            else:
-                if remaining < (num - result):
-                    num -= remaining
-                    total = (result * i) + num
-                    result = math.ceil(total / (i+1))
-                    remaining = result * (i+1) - total
-                else:
-                    remaining -= (num - result)
+            total += num
+            result = max(result, math.ceil(total / (i+1)))
         return result
