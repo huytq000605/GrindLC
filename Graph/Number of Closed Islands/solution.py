9243e03ad3a275bb86524dbdc456b437f78b5d52
huytq <huytq000605@gmail.com>
2023-04-06 20:26:56 +0800

    Number of closed Islands

--- Graph/Number of Closed Islands/solution.py
+++ Graph/Number of Closed Islands/solution.py
@@ -1,33 +1,27 @@
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
+        ds = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        m, n = len(grid), len(grid[0])
+        seen = [[0 for j in range(n)] for i in range(m)]
+        def dfs(r, c):
+            seen[r][c] = 1
+            for dr, dc in ds:
+                nr, nc = r + dr, c + dc
+                if nr < 0 or nc < 0 or nr >= m or nc >= n: continue
+                if grid[nr][nc] or seen[nr][nc]: continue
+                dfs(nr, nc)
+        
+        for r in [0, m-1]:
+            for c in range(n):
+                if grid[r][c] == 0: dfs(r, c)
+        for r in range(m):
+            for c in [0, n-1]:
+                if grid[r][c] == 0: dfs(r, c)
         result = 0
-        seen = [[False for j in range(n)] for i in range(m)]
-        dirs = [[0,1], [1,0], [-1,0], [0,-1]]
-        
-        def dfs(row, col):
-            seen[row][col] = True
-            for d in dirs:
-                nrow = row + d[0]
-                ncol = col + d[1]
-                if nrow < 0 or nrow >= m or ncol < 0 or ncol >= n or seen[nrow][ncol] == True or grid[nrow][ncol] == 1:
-                    continue
-                dfs(nrow, ncol)
-            
-        for row in range(m):
-            for col in [0, n-1]:
-                if grid[row][col] == 0:
-                    dfs(row, col)
-        
-        for col in range(n):
-            for row in [0, m-1]:
-                if grid[row][col] == 0:
-                    dfs(row, col)
-
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 0 and not seen[i][j]:
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == 0 and seen[r][c] == 0:
+                    dfs(r, c)
                     result += 1
-                    dfs(i, j)
         return result
+        
