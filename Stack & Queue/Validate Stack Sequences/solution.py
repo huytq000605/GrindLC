dcfabbfdb89ba0579f707fd31eaacef4b8452c81
huytq <huytq000605@gmail.com>
2023-04-13 22:54:21 +0800

    Validate Stack Sequence

--- Stack & Queue/Validate Stack Sequences/solution.py
+++ Stack & Queue/Validate Stack Sequences/solution.py
@@ -1,13 +1,15 @@
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
         i, j = 0, 0
+        n = len(pushed)
         stack = []
-        while j < len(popped):
-            while i < len(pushed) and (len(stack) == 0 or popped[j] != stack[-1]):
+        while j < n:
+            if stack and stack[-1] == popped[j]:
+                j += 1
+                stack.pop()
+            elif i < n:
                 stack.append(pushed[i])
                 i += 1
-            if stack[-1] != popped[j]:
+            else:
                 return False
-            stack.pop()
-            j += 1
         return True
