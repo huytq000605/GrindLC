985ed8a4de685392c2646a8c97657acb99bc048f
huytq <huytq000605@gmail.com>
2023-04-15 15:30:40 +0800

    Maximum Value of K Coins From Piles

--- Dynamic Programming/Maximum Value of K Coins From Piles/solution.py
+++ Dynamic Programming/Maximum Value of K Coins From Piles/solution.py
@@ -1,15 +1,16 @@
 class Solution:
     def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
+        n = len(piles)
         # dfs(i, k) have (k+1) transactions till the end => Time complexity: len(piles) * k 
         @cache
         def dfs(i, k):
-            if k == 0 or i >= len(piles):
-                return 0
+            if i >= n: return 0
+            value = 0
+            m = len(piles[i])
             result = 0
-            score = 0
-            for coins in range(min(len(piles[i]) + 1, k + 1)):
+            for coins in range(min(k, m) + 1):
                 if coins > 0:
-                    score += piles[i][coins - 1]
-                result = max(result, score + dfs(i + 1, k - coins))
+                    value += piles[i][coins-1]
+                result = max(result, value + dfs(i + 1, k - coins))
             return result
         return dfs(0, k)
