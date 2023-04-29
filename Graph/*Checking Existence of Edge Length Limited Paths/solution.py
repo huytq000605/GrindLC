5374d3d42d2594a57d550c16b729245a637d8ec7
huy.tran <huytq000605@gmail.com>
2023-04-30 00:00:36 +0700

    Checking Existence of Edge Length Limited Paths

--- Graph/*Checking Existence of Edge Length Limited Paths/solution.py
+++ Graph/*Checking Existence of Edge Length Limited Paths/solution.py
@@ -1,40 +1,33 @@
-class UnionFind:
+class UF:
     def __init__(self, n):
         self.p = [i for i in range(n)]
-        self.r = [1 for i in range(n)]
+        self.r = [1 for _ in range(n)]
     
-    def find(self, x):
-        if self.p[x] != x:
-            self.p[x] = self.find(self.p[x])
-        return self.p[x]
+    def find(self, u):
+        if u != self.p[u]:
+            self.p[u] = self.find(self.p[u])
+        return self.p[u]
     
-    def union(self, x, y):
-        x_p, y_p = self.find(x), self.find(y)
-        if x_p == y_p:
-            return
-        if self.r[x_p] < self.r[y_p]:
-            x_p, y_p = y_p, x_p
-        self.r[x_p] += self.r[y_p]
-        self.p[y_p] = x_p
+    def union(self, u, v):
+        u, v = self.find(u), self.find(v)
+        if u == v: return
+        if self.r[u] < self.r[v]: u, v = v, u
+        self.r[u] += self.r[v]
+        self.p[v] = u
 
 class Solution:
     def distanceLimitedPathsExist(self, n: int, edgeList: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        for i in range(len(queries)):
-            queries[i].append(i)
-        queries.sort(key = lambda query: query[2])
-        edgeList.sort(key = lambda edge: edge[2])
-        
-        uf = UnionFind(n)
-        
-        
-        idx = 0
-        result = [False] * len(queries)
+        edgeList.sort(key = lambda e: e[2])
+        queries = sorted([(query[2], query[0], query[1], i) for i, query in enumerate(queries)])
+        result = [False for _ in range(len(queries))]
+        i = 0
+        uf = UF(n)
         for query in queries:
-            u, v, limit, i = query
-            while idx < len(edgeList) nd edgeList[idx][2] < limit:
-                uf.union(edgeList[idx][0], edgeList[idx][1])
-                idx += 1
+            limit, u, v, idx = query
+            while i < len(edgeList) and edgeList[i][2] < limit:
+                a, b, _ = edgeList[i]
+                uf.union(a, b)
+                i += 1
             if uf.find(u) == uf.find(v):
-                result[i] = True
-        
+                result[idx] = True
         return result
