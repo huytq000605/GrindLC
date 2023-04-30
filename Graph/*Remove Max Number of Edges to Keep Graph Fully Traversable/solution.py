9980ee3c7920325caf010e2019a840edf4a1df07
huy.tran <huytq000605@gmail.com>
2023-04-30 15:48:33 +0700

    Rremove Max Number of Edges To Keep Graph Fully Traversable

--- Graph/*Remove Max Number of Edges to Keep Graph Fully Traversable/solution.py
+++ Graph/*Remove Max Number of Edges to Keep Graph Fully Traversable/solution.py
@@ -1,47 +1,51 @@
 class UF:
-    def __init__(self, size):
-        self.p = [i for i in range(size)]
-        self.r = [1 for i in range(size)]
+    def __init__(self, n):
+        self.p = [i for i in range(n)]
+        self.r = [1 for _ in range(n)]
     
-    def find(self, x):
-        if x != self.p[x]:
-            self.p[x] = self.find(self.p[x])
-        return self.p[x]
+    def find(self, u):
+        if u != self.p[u]:
+            self.p[u] = self.find(self.p[u])
+        return self.p[u]
     
     def union(self, u, v):
         u, v = self.find(u), self.find(v)
-        if u == v:
-            return False
-        if self.r[u] < self.r[v]:
-            u, v = v, u
+        if u == v: return False
+        if self.r[u] < self.r[v]: u, v = v, u
         self.p[v] = u
         self.r[u] += self.r[v]
         return True
+    
+    def clone(self):
+        other = UF(0)
+        other.p = [*self.p]
+        other.r = [*self.r]
+        return other
+    
 
 class Solution:
     def maxNumEdgesToRemove(self, n: int, edges: List[List[int]]) -> int:
-        uf1, uf2 = UF(n+1), UF(n+1)
-        edges1 = []
-        edges2 = []
-        used = 0
+        A, B, C = [], [], []
         for t, u, v in edges:
-            if t == 3:
-                use = False
-                use = uf1.union(u, v) or use
-                use = uf2.union(u, v) or use
-                if use: 
-                    used += 1
-            elif t == 1:
-                edges1.append((u, v))
-            else:
-                edges2.appnd((u, v))
-        for u, v in edges1:
-            if uf1.union(u, v):
-                used += 1
-        for u, v in edges2:
-            if uf2.union(u, v):
-                used += 1
-        if max(uf1.r) != n or max(uf2.r) != n:
-            return -1
+            u, v = u-1, v-1
+            if t == 1: A.append((u, v))
+            elif t == 2: B.append((u, v))
+            else: C.append((u, v))
+        result = 0
+        uf = UF(n)
+        for u, v in C:
+            if not uf.union(u, v):
+                result += 1
         
-        return len(edges) - used
+        ufA = uf.clone()
+        for u, v in A:
+            if not ufA.union(u, v):
+                result += 1
+        if ufA.r[ufA.find(0)] != n: return -1 
+        
+        ufB = uf.clone()
+        for u, v in B:
+            if not ufB.union(u, v):
+                result += 1
+        if ufB.r[ufB.find(0)] != n: return -1 
+        return result
