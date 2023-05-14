280fdc0a8bc3ae6f2e5478398f00bf528fcd9b95
huytq <huytq000605@gmail.com>
2023-05-14 15:50:43 +0800

    Maximize Score After N Operations

--- Dynamic Programming/Maximize Score After N Operations/solution.py
+++ Dynamic Programming/Maximize Score After N Operations/solution.py
@@ -1,26 +1,24 @@
-from functools import cache
-import math
-from typing import *
-
 class Solution:
     def maxScore(self, nums: List[int]) -> int:
         n = len(nums)
         @cache
-        def dfs(states):
-            if states == (1 << n) - 1:
-                return 0
-            i = 0
-            for j in range(n):
-                i += (states >> j) & 1
-            i = i // 2 + 1
+        def dfs(mask):
+            set_bit = 0
+            for i in range(n):
+                if (mask >> i) & 1:
+                    set_bit += 1
+            
             result = 0
-            for j in range(n):
-                if (states >> j) & 1 == 1:
+            
+            turn = set_bit // 2 + 1
+            for i in range(n):
+                if (mask >> i) & 1:
                     continue
-                for k in range(j + 1, n):
-                    if (states >> k) & 1 == 1:
+                for j in range(n):
+                    if (mask >> j) & 1 or j == i:
                         continue
-                    nextStates = states | (1 << j) | (1 << k)
-                    result = max(result, i * math.gcd(nums[j], nums[k]) + dfs(nextStates))
+                    next_mask = mask | (1 << i) | (1 << j)
+                    result = max(result, math.gcd(nums[i], nums[j]) * turn + dfs(next_mask))
             return result
         return dfs(0)
+                
