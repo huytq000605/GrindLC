29cb6fc06c7a8b70447f432a70687e5ef8dc638e
huytq <huytq000605@gmail.com>
2023-05-19 20:41:17 +0800

    Is Graph Bipartite?

--- Graph/Is Graph Bipartite?/solution.py
+++ Graph/Is Graph Bipartite?/solution.py
@@ -1,22 +1,17 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         n = len(graph)
-        set_of = [-1 for i in range(n)]
-        
-        def dfs(node, s):
-            if set_of[node] == s:
-                return True
-            elif set_of[node] == -1:
-                set_of[node] = s
-            else:
-                return False
-            for next_node in graph[node]:
-                if not dfs(next_node, 1-s):
-                    return False
-            return True
-        
-        for i in range(n):
-            if set_of[i] == -1:
-                if not dfs(i, 0):
-                    return False
+        colors = [0 for _ in range(n)]
+        def dfs(u, c):
+            if colors[u]:
+                if c != colors[u]: return 0
+                return c
+            colors[u] = c
+            for v in graph[u]:
+                if not dfs(v, 3-c):
+                    return 0
+            return c
+        for u in range(n):
+            if not colors[u]:
+                if not dfs(u, 1): return 0
         return True
