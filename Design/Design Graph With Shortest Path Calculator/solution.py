6e320f5c4447f640a98327a133d60f07ca536173
huytq <huytq000605@gmail.com>
2023-06-01 21:25:39 +0800

    Design Graph With Shortest Path Calculator

---
+++ Design/Design Graph With Shortest Path Calculator/solution.py
@@ -0,0 +1,30 @@
+class Graph:
+
+    def __init__(self, n: int, edges: List[List[int]]):
+        self.graph = [[] for _ in range(n)]
+        self.n = n
+        for u, v, c in edges:
+            self.graph[u].append((v, c))
+        
+
+    def addEdge(self, edge: List[int]) -> None:
+        u, v, c = edge
+        self.graph[u].append((v, c))
+
+    def shortestPath(self, node1: int, node2: int) -> int:
+        pq = [(0 ,node1)]
+        d = [math.inf for _ in range(self.n)]
+        while pq:
+            s, u = heappop(pq)
+            if u == node2:
+                return s
+            for v, c in self.graph[u]:
+                if s + c < d[v]:
+                    d[v] = s + c
+                    heappush(pq, (s + c, v))
+        return -1
+
+# Your Graph object will be instantiated and called as such:
+# obj = Graph(n, edges)
+# obj.addEdge(edge)
+# param_2 = obj.shortestPath(node1,node2)
