5e62bc8db5537f6ca00573ee50d54b7f37496929
huytq <huytq000605@gmail.com>
2023-06-09 20:24:43 +0800

    Find Smallest letter Greater Than Target

---
+++ Easy Guys/Find Smallest Letter Greater Than Target/solution.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn next_greatest_letter(letters: Vec<char>, target: char) -> char {
+        for &letter in letters.iter() {
+            if letter > target {
+                return letter
+            }
+        }
+        return letters[0]
+    }
+}
