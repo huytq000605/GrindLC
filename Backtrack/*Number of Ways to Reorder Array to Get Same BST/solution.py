3900c598a40f05b3941bb2f1038b8b9fd26e461c
huy.tran <huytq000605@gmail.com>
2023-06-16 11:17:44 +0700

    Number of Ways to Reorder Array to Get Same BST

--- Backtrack/*Number of Ways to Reorder Array to Get Same BST/solution.py
+++ Backtrack/*Number of Ways to Reorder Array to Get Same BST/solution.py
@@ -1,21 +1,9 @@
 class Solution:
-        
     def numOfWays(self, nums: List[int]) -> int:
         MOD = 10**9 + 7
         def ways(nums):
-            if len(nums) <= 2:
-                return 1
-            left = []
-            right = []
-            for i in range(1, len(nums)):
-                if nums[i] < nums[0]:
-                    left.append(nums[i])
-                else:
-                    right.append(nums[i])
-            leftNodes = len(left)
-            rightNodes = len(right)
-            totalNodes = leftNodes + rightNodes
-			# combination is nCk, we put leftNodes into totalNodes
-            return ways(left) * ways(right) * math.comb(totalNodes, leftNodes)
-        return (ways(nums) - 1) % MOD
-        
+            if len(nums) <= 1: return 1
+            left = list(filter(lambda num: num < nums[0], nums))
+            right = list(filter(lambda num: num > nums[0], nums))
+            return ways(left) % MOD * ways(right) % MOD * math.comb(len(left) + len(right), len(left)) % MOD 
+        return (ways(nums) - 1)
