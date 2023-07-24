13716667f7a8979d90e21b3229fdbb320f9a9b39
huytq <huytq000605@gmail.com>
2023-07-24 19:51:20 +0800

    Pow(x,n)

---
+++ Math & Game Theory & Simulation/Pow(x, n)/solution.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn my_pow(x: f64, n: i32) -> f64 {
+        if n == 0 {
+            return 1.0
+        }
+        if n % 2 == 0 {
+            return Solution::my_pow(x, n/2) * Solution::my_pow(x, n/2)
+        } else {
+            if n > 0 {
+                Solution::my_pow(x, n/2) *  Solution::my_pow(x, n/2) * x
+            } else {
+                Solution::my_pow(x, n/2) *  Solution::my_pow(x, n/2) / x
+            }
+        }
+    }
+}
