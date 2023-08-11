2360fe3d1cd358a1cafc2880a9d9b4181ca6c5e3
huytq <huytq000605@gmail.com>
2023-08-11 20:58:45 +0800

    Coin Change 2

--- Dynamic Programming/Coin Change 2/solution.py
+++ Dynamic Programming/Coin Change 2/solution.py
@@ -1,13 +1,25 @@
-from typing import List, Tuple
-from functools import lru_cache
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        @lru_cache(maxsize = None)
-        def helper(amount: int, coins: Tuple) -> int:
-            if amount == 0:
-                return 1
-            if amount < 0 or len(coins) == 0:
+        # TOP DOWN
+        @cache
+        def dfs(i, coin):
+            if i >= len(coins):
+                if coin == amount:
+                    return 1
                 return 0
-            return helper(amount - coins[-1], coins) + helper(amount, coins[0:-1])
-        return helper(amount, tuple(coins))
+            if coin > amount:
+                return 0
+            # dp[i][j] = dp[i][j+coins[i]] + dp[i+1][j]
+            return dfs(i, coin + coins[i]) + dfs(i+1, coin)
         
+        # BOTTOM UP
+        n = len(coins)
+        dp = [[0 for _ in range(amount+1)] for _ in range(n + 1)]
+        dp[n][amount] = 1
+        for i in reversed(range(n)):
+            for j in reversed(range(amount+1)):
+                dp[i][j] = dp[i+1][j]
+                if j + coins[i] > amount: continue
+                dp[i][j] += dp[i][j + coins[i]]
+        
+        return dp[0][0]
