8621fe041a90f62fc9f00d8e17fe799e84fa7d83
huytq <huytq000605@gmail.com>
2023-09-04 20:32:31 +0800

    Check if a String Is an Acryonum of Words

---
+++ Easy Guys/Check if a String Is an Acronym of Words/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAcronym(self, words: List[str], s: str) -> bool:
+        if len(words) != len(s):
+            return False
+        for i, w in enumerate(words):
+            if w[0] != s[i]:
+                return False
+        return True
+            
+            
