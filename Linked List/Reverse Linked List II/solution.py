bd1d0b99dd99b69d88374b3642f6ddaec9664299
huytq <huytq000605@gmail.com>
2023-09-07 21:30:55 +0800

    Reverse Linked List II

--- Linked List/Reverse Linked List II/solution.py
+++ Linked List/Reverse Linked List II/solution.py
@@ -5,27 +5,33 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if left == right:
+            return head
+        
         prev, cur = None, head
-        position = 1
-        while cur:
-            if position == left:
-                prev_reverse = prev
-                start_reverse = cur
-                prev = None
-                while cur and position != right + 1:
-                    nxt = cur.next
-                    cur.next = prev
-                    prev = cur
-                    cur = nxt
-                    position += 1
-                start_reverse.next = cur
-                if prev_reverse:
-                    prev_reverse.next = prev
-                break
+        i = 1
+        while i < left:
             prev = cur
             cur = cur.next
-            position += 1
+            i += 1
+        
+        prev_reverse = prev
+        start_reverse = cur
+        while i <= right:
+            nxt = cur.next
+            cur.next = prev
+            
+            prev = cur
+            cur = nxt
+            i += 1
+        
+        if prev_reverse:
+            prev_reverse.next = prev
+        start_reverse.next = cur
+        
         if left == 1:
             return prev
-        else:
-            return head
+        return head
+                    
+                    
+                
