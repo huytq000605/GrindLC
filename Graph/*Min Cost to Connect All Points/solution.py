f5de66da39a2d4744d5d0e0da8506fbb87c71b55
huytq <huytq000605@gmail.com>
2023-09-15 22:21:20 +0800

    Min Cost to Connect All Points

--- Graph/*Min Cost to Connect All Points/solution.py
+++ Graph/*Min Cost to Connect All Points/solution.py
@@ -1,42 +1,38 @@
+class UF:
+    def __init__(self, n):
+        self.p = [i for i in range(n)]
+        self.r = [1 for _ in range(n)]
+    
+    def find(self, u):
+        if self.p[u] != u:
+            self.p[u] = self.find(self.p[u])
+        return self.p[u]
+    
+    def union(self, u, v):
+        u, v = self.find(u), self.find(v)
+        if u == v:
+            return 0
+        if self.r[u] < self.r[v]:
+            u, v = v, u
+        self.r[u] += self.r[v]
+        self.p[v] = u
+        return self.r[u]
+
 class Solution:
-    class UnionFind:
-        def __init__(self, n):
-            self.p = [i for i in range(n)]
-            self.r = [1 for i in range(n)]
-        
-        def find(self, x):
-            if x != self.p[x]:
-                self.p[x] = self.find(self.p[x])
-            return self.p[x]
-        
-        def union(self, x, y):
-            x_p, y_p = self.find(x), self.find(y)
-            if x_p == y_p:
-                return 0
-            if self.r[x_p] < self.r[y_p]:
-                x_p, y_p = y_p, x_p
-            self.p[y_p] = x_p
-            self.r[x_p] += self.r[y_p]
-            return self.r[x_p]
-            
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
-        if n <= 1:
-            return 0
-        distances = []
+        uf = UF(n)
+        pq = []
         for i in range(n):
             x1, y1 = points[i]
-            for j in range(i + 1, n):
+            for j in range(i+1, n):
                 x2, y2 = points[j]
                 distance = abs(x1 - x2) + abs(y1 - y2)
-                heappush(distances, (distace, i, j))
+                heappush(pq, (distance, i, j))
         result = 0
-        uf = self.UnionFind(n)
-        while True:
-            d, u, v = heappop(distances)
-            value = uf.union(u, v)
-            if value != 0:
-                result += d
-            if value == n:
-                return result
-        n
+        while pq:
+            d, u, v = heappop(pq)
+            val = uf.union(u, v)
+            if val != 0: result += d
+            if val == n: return result
+        return result
