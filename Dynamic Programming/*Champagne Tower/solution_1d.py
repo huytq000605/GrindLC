f2bcf7ebe8c1b1696711a174e8886e46abb27e4e
huytq <huytq000605@gmail.com>
2023-09-24 15:44:29 +0800

    Champagne Tower

--- Dynamic Programming/*Champagne Tower/solution_1d.py
+++ Dynamic Programming/*Champagne Tower/solution_1d.py
@@ -1,21 +1,12 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        dp = [0 for i in range(query_row + 1)]
-        
-        dp[0] = poured
-        
-        for row in range(1, query_row + 1):
-            nextDp = [0 for i in range(query_row + 1)]
-            for col in range(0, row + 1):
-                dp[col] -= 1
-                if dp[col] > 0:
-                    nextDp[col] += dp[col] / 2
-                if col > 0:
-                    if dp[col-1] > 0:
-                        nextDp[col] += dp[col-1] / 2
-            dp = nextDp
-        
-        if dp[query_glass] > 1:
-            return 1.0
-        
-        return dp[query_glass]
+        dp = [poured]
+        for row in range(1, query_row+1):
+            next_dp = [0 for _ in range(row + 1)]
+            for col in range(row):
+                if dp[col] < 1: continue
+                next_dp[col] += (dp[col] - 1)/2
+                next_dp[col+1] += (dp[col] - 1)/2
+                
+            dp = next_dp
+        return min(1.0, dp[query_glass])
