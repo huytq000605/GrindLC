638f2de9ab009b7c52577adf94f66aae09f0ab21
huy.tran <huytq000605@gmail.com>
2023-10-06 23:49:33 +0700

    Integer Break

--- Dynamic Programming/Integer Break/solution_bottom_up.py
+++ Dynamic Programming/Integer Break/solution_bottom_up.py
@@ -1,11 +1,11 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        dp = [i for i in range(n)]
-        dp[0] = 1
-        for i in range(2, n):
-            for j in range(1, i + 1):
-                dp[i] = max(dp[i], dp[j] * dp[i - j])
+        # dp[i] = maximum of product if use i as the break one
+        dp = [i for i in range(n + 1)]
+        for i in range(2, n+1):
+            for j in range(1, i):
+                dp[i] = max(dp[i], dp[j] * dp[i-j])
         result = 0
         for i in range(1, n):
-            result = max(result, dp[i] * dp[n - i])
+            result = max(result, dp[i] * dp[n-i])
         return result
