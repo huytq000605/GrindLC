a1bf74b6bdb46d1501ccc91f8c9625a43cbe185c
huytq <huytq000605@gmail.com>
2023-10-11 17:53:45 +0800

    Bus Routes

--- Graph/Bus Routes/solution.py
+++ Graph/Bus Routes/solution.py
@@ -1,17 +1,18 @@
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
-        graph = defaultdict(list)
-        for bus, bus_stops in enumerate(routes):
-            for bus_stop in bus_stops:
-                graph[bus_stop].append(bus)
-        q = deque([(source, 0)])
-        while q:
-            bus, step = q.popleft()
-            if bus == target:
-                return step
-            for bus_stop in graph[bus]:
-                for next_bus in routes[bus_stop]:
-                    q.append((next_bus, step + 1))
-                routes[bus_stop] = []
-            graph[bus] = []
+        buses = defaultdict(list)
+        for i, route in enumerate(routes):
+            for bus in route:
+                buses[bus].append(i)
+        
+        dq = deque([(source, 0)])
+        while dq:
+            u, s = dq.popleft()
+            if u == target:
+                return s
+            for route in buses[u]:
+                for v in routes[route]:
+                    dq.append((v, s+1))
+                routes[route] = []
+            buses[u] = []
         return -1
