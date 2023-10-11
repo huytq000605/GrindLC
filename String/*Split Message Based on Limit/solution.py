d48ec6a1fd5fdc9c6959b556748c84d22e51f753
huytq <huytq000605@gmail.com>
2023-10-11 17:37:49 +0800

    Split Message Based on Limit

--- String/*Split Message Based on Limit/solution.py
+++ String/*Split Message Based on Limit/solution.py
@@ -2,18 +2,22 @@ class Solution:
     def splitMessage(self, message: str, limit: int) -> List[str]:
         n = len(message)
         parts = 0
-        idxs = 0
-        while idxs + (3 + len(str(parts))) * parts + n > limit * parts:
-            if 3 + len(str(parts)) * 2 > limit:
+        prefix = 0
+        # <prefix/suffix>
+        # 3 is space for </>
+        while prefix + (3 + len(str(parts))) * parts + len(message) > parts * limit:
+            # if the space of part annotation is gte to limit than no space for message
+            if len(str(parts)) * 2 + 3 >= limit:
                 return []
             parts += 1
-            idxs += len(str(parts))
+            # each time parts increase by 1, prefix inscreae by len(str(parts))
+            prefix += len(str(parts))
         
         result = []
         idx = 0
-        for i in range(parts):
-            suffix = f"<{i+1}/{parts}>"
-            prefix = message[idx:idx + limit - len(suffix)]
-            idx = idx + limit - len(suffix)
-            result.append(prefix + suffix)
+        for part in range(parts):
+            annotation = f"<{part+1}/{parts}>"
+            msg = message[idx:idx+limit-len(annotation)]
+            idx += len(msg)
+            result.append(msg+annotation)
         return result
