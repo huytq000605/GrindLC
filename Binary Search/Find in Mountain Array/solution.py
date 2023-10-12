2bdb88edbb3f12b2da5e7a6790fb3d15bbc4e4a2
huytq <huytq000605@gmail.com>
2023-10-12 20:37:05 +0800

    Find in Mountain Array

--- Binary Search/Find in Mountain Array/solution.py
+++ Binary Search/Find in Mountain Array/solution.py
@@ -10,45 +10,50 @@ class Solution:
     def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
         n = mountain_arr.length()
         @cache
-        def get(idx):
-            return mountain_arr.get(idx)
+        def get(n):
+            return mountain_arr.get(n)
         
-        
-        left, right = 1, n - 2
-        mid = left + (right - left) // 2
-        while not (get(mid) > get(mid - 1) and get(mid) > get(mid + 1)):
-            if get(mid - 1) > get(mid + 1):
-                right = mid - 1
-                mid = left + (right - left) // 2
-            else:
-                left = mid + 1
-                mid = left + (right - left) // 2
-        peak = mid
-        
+        start = 1
+        end = n-2
+        while start < end:
+            i = start + (end - start) // 2
+            a = get(i-1)
+            b = get(i)
+            if a > b:
+                end = i-1
+                continue
+            c = get(i+1)
+            if a < b and b > c:
+                start = i
+                break
+            if c > b:
+                start = i+1
+                continue
+        peak = start
+
         start = 0
         end = peak
         while start < end:
             mid = start + (end - start) // 2
-            value = get(mid)
-            if value < target:
+            if get(mid) < target:
                 start = mid + 1
             else:
                 end = mid
-                
         if get(start) == target:
             return start
         
         start = peak + 1
-        end = n - 1
+        end = n-1
         while start < end:
             mid = start + (end - start) // 2
-            value = get(mid)
-            if value > target:
+            if get(mid) > target:
                 start = mid + 1
             else:
                 end = mid
-                
         if get(start) == target:
             return start
         
         return -1
+        
+        
+            
