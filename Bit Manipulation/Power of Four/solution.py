dc9f49da108f19e008b340131c90858c7acc52d5
huytq <huytq000605@gmail.com>
2023-10-23 21:13:08 +0800

    Power of Four

--- Bit Manipulation/Power of Four/solution.py
+++ Bit Manipulation/Power of Four/solution.py
@@ -1,6 +1,8 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         # n > 0
-        # Is power of 2
-        # Log2(n) % 2 == 0
-        return n > 0 and n & (n-1) == 0 and 0b1010101010101010101010101010101 & n == n
+        # n = 2^x
+        # x % 2 == 0
+        return n > 0 and \
+        n & (n-1) == 0 and \
+        0b101010101010101010101010101010101 & n == n
