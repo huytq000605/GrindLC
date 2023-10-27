b3f5dea8dadf08e9965e4865e6cfa24fd62418cf
huytq <huytq000605@gmail.com>
2023-10-27 19:49:08 +0800

    Longest Palindromic Substring

--- String/*Longest Palindromic Substring/solution.py
+++ String/*Longest Palindromic Substring/solution.py
@@ -1,20 +1,16 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
+        def extend(i, j):
+            while i >= 0 and j < n and s[i] == s[j]:
+                i -= 1
+                j += 1
+            return i+1, j-1
         result = ""
-        def extend(start, end):
-            while start >= 0 and end < n and s[start] == s[end]:
-                start -= 1
-                end += 1
-            return start + 1, end - 1
-
         for i in range(n):
-            start, end = extend(i, i)
-            if end - start + 1 > len(result):
-                result = s[start:end+1]
-
-            start, end = extend(i, i + 1)
-            if end - start + 1 > len(result):
-                result = s[start:end+1]
+            a, b = extend(i, i)
+            if b - a + 1 > len(result): result = s[a:b+1]
+            a, b = extend(i, i+1)
+            if b - a + 1 > len(result): result = s[a:b+1]
         return result
 
