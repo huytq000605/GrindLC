97a3282d77cc7867b9a8c176b51049ee01767054
huy.tran <huytq000605@gmail.com>
2023-11-11 13:06:31 +0700

    Design Graph With Shorest Path Calculator

--- Design/Design Graph With Shortest Path Calculator/solution.py
+++ Design/Design Graph With Shortest Path Calculator/solution.py
@@ -5,24 +5,25 @@ class Graph:
         self.n = n
         for u, v, c in edges:
             self.graph[u].append((v, c))
-        
 
     def addEdge(self, edge: List[int]) -> None:
         u, v, c = edge
         self.graph[u].append((v, c))
 
     def shortestPath(self, node1: int, node2: int) -> int:
-        pq = [(0 ,node1)]
-        d = [math.inf for _ in range(self.n)]
+        pq = [(0, node1)]
+        ds = [math.inf for _ in range(self.n)]
+        ds[node1] = 0
         while pq:
             s, u = heappop(pq)
             if u == node2:
                 return s
             for v, c in self.graph[u]:
-                if s + c < d[v]:
-                    d[v] = s + c
-                    heappush(pq, (s + c, v))
+                if s + c < ds[v]:
+                    ds[v] = s + c
+                    heappush(pq, (s+c, v))
         return -1
+
 
 # Your Graph object will be instantiated and called as such:
 # obj = Graph(n, edges)
