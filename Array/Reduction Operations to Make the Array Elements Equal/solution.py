4a182fa93c6866d99fdac21f884909cf668fb9ed
huytq <huytq000605@gmail.com>
2023-11-19 16:37:25 +0800

    Reduction Operations to Make the Array Elements Equa

--- Array/Reduction Operations to Make the Array Elements Equal/solution.py
+++ Array/Reduction Operations to Make the Array Elements Equal/solution.py
@@ -1,11 +1,10 @@
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
         nums.sort(reverse = True)
-        numOfNumber = 0
         result = 0
+        cur = 0
         for i in range(len(nums)):
-            if i > 0 and nums[i] != nums[i - 1]:
-                result += numOfNumber
-            numOfNumber += 1
+            if i > 0 and nums[i] < nums[i-1]:
+                result += cur
+            cur += 1
         return result
-                
--- Array/Reduction Operations to Make the Array Elements Equal/solution_O(n)_space.py
+++ Array/Reduction Operations to Make the Array Elements Equal/solution_O(n)_space.py
@@ -1,13 +1,9 @@
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
-        freq = dict()
-        for num in nums:
-            if num not in freq: freq[num] = 0
-            freq[num] += 1
-        values = sorted(freq.items(), reverse = True)
-        numOfNumber = 0
+        counter = Counter(nums)
+        cur = 0
         result = 0
-        for i in range(len(values)):
-            result += numOfNumber
-            numOfNumber += values[i][1]
+        for k in sorted(counter.keys(), reverse = True):
+            result += cur
+            cur += counter[k]
         return result
