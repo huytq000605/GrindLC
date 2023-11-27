d6b059da253eb2dd78010dbc2acf67450daee4eb
huytq <huytq000605@gmail.com>
2023-11-27 20:43:21 +0800

    Maximum Strong Pair XOR I

---
+++ Easy Guys/Maximum Strong Pair XOR I/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumStrongPairXor(self, nums: List[int]) -> int:
+        n = len(nums)
+        max_xor = 0
+        for i in range(n):
+            for j in range(n):
+                if abs(nums[i] - nums[j]) <= min(nums[i], nums[j]):
+                    max_xor = max(max_xor, nums[i] ^ nums[j])
+
+        return max_xor
