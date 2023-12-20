778b7a7e8cf3270c7474e8db308ea75e2918c646
huy.tran <huytq000605@gmail.com>
2023-12-20 17:31:01 +0700

    Buy Two Chocolates

--- Easy Guys/Buy Two Chocolates/solution.py
+++ Easy Guys/Buy Two Chocolates/solution.py
@@ -1,6 +1,11 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        prices.sort()
-        if prices[0] + prices[1] > money:
+        pq = []
+        for price in prices:
+            heappush(pq, -price)
+            if len(pq) > 2:
+                heappop(pq)
+        if -sum(pq) <= money:
+            return money + sum(pq)
+        else:
             return money
-        return money - prices[0] - prices[1]
