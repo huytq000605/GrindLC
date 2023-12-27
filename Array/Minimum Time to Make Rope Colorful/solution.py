c0e2ae0ab9dd5368386dc989cab5f720e4560e03
huy.tran <huytq000605@gmail.com>
2023-12-27 09:30:32 +0700

    Minimum Time to Make Rope Colorful

--- Array/Minimum Time to Make Rope Colorful/solution.py
+++ Array/Minimum Time to Make Rope Colorful/solution.py
@@ -1,14 +1,15 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
+        cur = ""
+        pq = []
         result = 0
-        prev = "#"
-        max_cost = 0
         for i, color in enumerate(colors):
-            cost = neededTime[i]
-            if color == prev:
-                result += min(max_cost, cost)
-                max_cost = max(max_cost, cost)
-            else:
-                prev = color
-                max_cost = cost
+            if color != cur:
+                pq = []
+            cur = color
+            heappush(pq, neededTime[i])
+            if len(pq) > 1:
+                result += heappop(pq)
         return result
+
+
