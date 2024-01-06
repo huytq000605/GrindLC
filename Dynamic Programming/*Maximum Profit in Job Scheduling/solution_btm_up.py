d43a9efb4776dfb2cfad60d6ed593ee726487ad1
huytq <huytq000605@gmail.com>
2024-01-06 16:12:29 +0800

    Maximum Profit in Job Scheduling

--- Dynamic Programming/*Maximum Profit in Job Scheduling/solution_btm_up.py
+++ Dynamic Programming/*Maximum Profit in Job Scheduling/solution_btm_up.py
@@ -3,21 +3,19 @@ class Solution:
         n = len(starts)
         jobs = [(starts[i], ends[i], profits[i]) for i in range(n)]
         jobs.sort()
-        @cache
-        def dfs(i):
-            if i >= n:
-                return 0
-            result = dfs(i+1)
-            # j as the index of the next job
-            # jobs[j][0] >= ends[i]
-            start = i + 1
-            end = n
+        # dp[i] means the maximum of profit we can get when process range(i, n)
+        dp = [0 for _ in range(n)]
+        dp[-1] = jobs[-1][2]
+        for i in range(n-2, -1, -1):
+            start, end = i+1, n
             while start < end:
                 mid = start + (end - start) // 2
                 if jobs[mid][0] >= jobs[i][1]:
                     end = mid
                 else:
                     start = mid + 1
-            result = max(result, dfs(start) + jobs[i][2])
-            return result
-        return dfs(0)
+            nxt_job = 0
+            if start < n:
+                nxt_job = dp[start]
+            dp[i] = max(nxt_job + jobs[i][2], dp[i + 1])
+        return dp[0]
