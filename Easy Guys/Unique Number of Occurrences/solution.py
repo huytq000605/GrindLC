bd44b56e9a42a6b88dd8004cc6a80b88b16c8829
huytq <huytq000605@gmail.com>
2024-01-17 15:18:38 +0800

    Unique Number of Occurrences

--- Easy Guys/Unique Number of Occurrences/solution.py
+++ Easy Guys/Unique Number of Occurrences/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         counter = Counter(arr)
-        counterFreq = Counter(counter.values())
-        for freq_freq in counterFreq.values():
+        freq_counter = Counter(counter.values())
+        for freq_freq in freq_counter.values():
             if freq_freq > 1:
                 return False
         return True
