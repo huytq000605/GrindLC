2c45f603b0357129505ac04e4cb7b36f9464dac4
huytq <huytq000605@gmail.com>
2024-01-19 21:04:24 +0800

    Minimum Failling Path Sum

--- Dynamic Programming/Minimum Falling Path Sum/solution.py
+++ Dynamic Programming/Minimum Falling Path Sum/solution.py
@@ -1,14 +1,11 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         m, n = len(matrix), len(matrix[0])
-        @cache
-        def dfs(r, c):
-            if r + 1 == m:
-                return 0
-            result = math.inf
-            for nc in range(c-1, c+2):
-                if nc < 0 or nc >= n:
-                    continue
-                result = min(result, dfs(r + 1, nc) + matrix[r+1][nc])
-            return result
-        return min([dfs(0, i) + matrix[0][i] for i in range(n)])
+        dp = [matrix[0][c] for c in range(n)]
+        for r in range(1, m):
+            ndp = [math.inf for _ in range(n)]
+            for c in range(n):
+                for dc in range(max(0,c-1), min(n, c+2)):
+                    ndp[c] = min(ndp[c], dp[dc] + matrix[r][c])
+            dp = ndp
+        return min(dp)
