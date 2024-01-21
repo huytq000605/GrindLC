283f8c770d7dc3dfce0faddfae3ae48253d4a19a
huytq <huytq000605@gmail.com>
2024-01-21 14:04:52 +0800

    House Robber

--- Dynamic Programming/House Robber/solution.py
+++ Dynamic Programming/House Robber/solution.py
@@ -1,9 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
         prev1, prev2 = 0, 0
         for num in nums:
-            cur = max(prev1, prev2 + num)
-            prev2 = prev1
-            prev1 = cur
-        return max(prev1, prev2)
+            prev1, prev2 = prev2, max(prev1 + num, prev2)
+        return prev2
