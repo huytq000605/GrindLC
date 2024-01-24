31c845c02d4b53ae7a1146d209f2b2ec27d399f6
huytq <huytq000605@gmail.com>
2024-01-24 21:20:05 +0800

    Pseudo-Palindromic Paths in a Binary Tree

--- Tree & Binary Tree/Pseudo-Palindromic Paths in a Binary Tree/solution.py
+++ Tree & Binary Tree/Pseudo-Palindromic Paths in a Binary Tree/solution.py
@@ -3,27 +3,36 @@
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
 #         self.right = right
 class Solution:
     def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
-        count = [0 for i in range(10)]
+        freq = [0 for _ in range(10)]
         result = 0
-        def dfs(node):
-            if not node:
-                return
-            nonlocal result
-            count[node.val] += 1
-            if not node.left and not node.right:
-                count_odd = 0
-                for i in range(10):
-                    if count[i] % 2 == 1:
-                        count_odd += 1
-                if count_odd <= 1:
-                    result += 1
-            dfs(node.left)
-            dfs(node.right)
-            count[node.val] -= 1
+
+        def pseudo_palindrome(freq):
+            odd = 0
+            for v in freq:
+                if v % 2 == 1:
+                    odd += 1
+                if odd > 1:
+                    return False
+            return True
+
+        def dfs(u):
+            nonlocal result, freq
+            freq[u.val] += 1
+            if not u.left and not u.right:
+                result += pseudo_palindrome(freq)
+            if u.left:
+                dfs(u.left)
+            if u.right:
+                dfs(u.right)
+            freq[u.val] -= 1
+        
         dfs(root)
-        return result
-                
-                            
+        return result#         self.right = right
