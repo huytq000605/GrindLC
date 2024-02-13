7cc6fb7cb71eb4935f9021e0e2ab4c5efb31e0d1
huy.tran <huytq000605@gmail.com>
2024-02-13 15:49:46 +0700

    Find First Palindromic String in the Array

--- Easy Guys/Find First Palindromic String in the Array/solution.py
+++ Easy Guys/Find First Palindromic String in the Array/solution.py
@@ -1,16 +1,15 @@
 class Solution:
     def firstPalindrome(self, words: List[str]) -> str:
-        def isPalindrome(s):
-            start = 0
-            end = len(s) - 1
-            while start < end:
-                if s[start] != s[end]:
+        def is_palindrome(s):
+            i, j = 0, len(s) - 1
+            while i < j:
+                if s[i] != s[j]:
                     return False
-                start += 1
-                end -= 1
+                i += 1
+                j -= 1
             return True
         
         for word in words:
-            if isPalindrome(word):
+            if is_palindrome(word):
                 return word
         return ""
