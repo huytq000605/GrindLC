1439d4f42b6928fb8fa574b9b9489caa1be4f58e
huy.tran <huytq000605@gmail.com>
2024-02-17 15:32:39 +0700

    Furthest Building You Can Reach

--- Priority Queue/Furthest Building You Can Reach/solution.py
+++ Priority Queue/Furthest Building You Can Reach/solution.py
@@ -4,16 +4,19 @@ class Solution:
         n = len(heights)
         for i in range(1, n):
             diff = heights[i] - heights[i-1]
-            if diff > 0:
-                if ladders:
-                    ladders -= 1
-                    heappush(pq, diff)
-                elif pq and pq[0] < diff and bricks >= pq[0]:
-                    bricks -= heappop(pq)
-                    heappush(pq, diff)
-                elif bricks >= diff:
-                    bricks -= diff
-                else:
-                    return i-1
+            if diff < 0:
+                continue
+            if ladders:
+                ladders -= 1
+                heappush(pq, diff)
+            elif pq and pq[0] < diff and pq[0] <= bricks:
+                bricks -= heappop(pq)
+                heappush(pq, diff)
+            elif diff <= bricks:
+                bricks -= diff
+            else:
+                return i-1
         return n-1
-
+            
+            
+            class Solution:
