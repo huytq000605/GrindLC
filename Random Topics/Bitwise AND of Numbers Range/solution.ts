b10db7caf2b905bc07e4c6c716b683a7bb4ca7cb
huytq <huytq000605@gmail.com>
2024-02-22 20:51:11 +0800

    Bitwise AND of Numbers Range

--- Random Topics/Bitwise AND of Numbers Range/solution.ts
+++
@@ -1,9 +0,0 @@
-function rangeBitwiseAnd(left: number, right: number): number {
-    let i = 0;
-    while(left !== right) {
-        left >>= 1
-        right >>= 1
-        i++
-    }
-    return left << i
-};
