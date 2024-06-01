f3b9740a7b9d2b102af862f14121ce79501c3131
huytq <huytq000605@gmail.com>
2024-06-02 03:08:07 +0800

    Valid Word Abbreviation

--- String/Valid Word Abbreviation/solution.cpp
+++ String/Valid Word Abbreviation/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool validWordAbbreviation(string word, string abbr) {
         int i = 0, j = 0;
-        while(i < abbr.size()) {
+        while(i < abbr.size() && j < word.size()) {
             if(abbr[i] == '0') return false;
             if(isdigit(abbr[i])) {
                 int k = 0;
@@ -13,11 +13,11 @@ public:
                 j += k;
                 if(j > word.size()) return false;
             } else {
-                if(j >= word.size() || abbr[i] != word[j]) return false;
+                if(abbr[i] != word[j]) return false;
                 i += 1;
                 j += 1;
             }
         }
-        return j == word.size();
+        return i == abbr.size() && j == word.size();
     }
 };
