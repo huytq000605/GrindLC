f4d1daf18f5f06e6337857a6d3a68315d885b82b
huytq <huytq000605@gmail.com>
2024-07-24 04:19:30 +0800

    Vowels Game in a String

---
+++ Easy Guys/Vowels Game in a String/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        for(auto c: s) {
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return true;
+        }
+        return false;
+    }
+};
