b154ac19456a3680429967ce2a896c6c429cd14c
huytq <huytq000605@gmail.com>
2024-08-22 23:17:03 +0800

    Number Complement

--- Bit Manipulation/Number Complement/solution.cpp
+++ Bit Manipulation/Number Complement/solution.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int findComplement(int num) {
-        string bin = "";
-        while(num > 0) {
-            bin = to_string(num % 2) + bin;
-            num /= 2;
-        }
         int result = 0;
-        for(int i = 0; i < bin.length(); i++) {
-            if(bin[i] == '0')
-                result += (1 << (bin.length() - i - 1));
+        int bit = 0;
+        while(num) {
+            result |= (1 - (num & 1)) << bit;
+            num >>= 1;
+            bit++;
         }
         return result;
     }
