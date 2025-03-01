a24803fabeaa59c92c5b1b8b0a522c3de1527f91
huytq <huytq000605@gmail.com>
2025-03-01 16:57:59 +0800

    Apply Operations to an Array

---
+++ Easy Guys/Apply Operations to an Array/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0, j = 0; i < n; ++i) {
+            if(i+1 < n && nums[i] == nums[i+1]) {
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+            if(nums[i]) swap(nums[j++], nums[i]);
+        }
+        return nums;
+    }
+};
