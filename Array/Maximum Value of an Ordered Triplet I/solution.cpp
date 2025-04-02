b531a10b3f21447c6f63d2bc04dc6330613e6bb1
huytq <huytq000605@gmail.com>
2025-04-02 19:16:21 +0800

    Maximum Value of an Ordered Triplet I

---
+++ Array/Maximum Value of an Ordered Triplet I/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        long long mx = nums[0], mx_diff = 0;
+        long long result = 0;
+        for(int i = 1; i < nums.size(); ++i) {
+            result = max(result, mx_diff * nums[i]);
+            mx_diff = max(mx_diff, mx - nums[i]);
+            mx = max(mx, static_cast<long long>(nums[i]));
+        }
+        return result;
+    }
+};
