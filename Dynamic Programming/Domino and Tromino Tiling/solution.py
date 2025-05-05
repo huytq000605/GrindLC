29b5ee608dec33aa243eb95629d25198ca3a03ab
huytq <huytq000605@gmail.com>
2025-05-05 16:41:03 +0800

    Domino and Tromino Tiling

--- Dynamic Programming/Domino and Tromino Tiling/solution.py
+++ Dynamic Programming/Domino and Tromino Tiling/solution.py
@@ -2,21 +2,11 @@ class Solution:
     def numTilings(self, n: int) -> int:
         MOD = 10**9 + 7
         @cache
-        def dfs(i, j):
-            print(i, j)
-            if i == n and j == n:
-                return 1
-            if i > n or j > n:
-                return 0
-            if j > i:
-                return dfs(j, i)
-            res = 0
-            if i - j == 1:
-                res += dfs(i + 1, j + 2)
-            if i == j:
-                res += dfs(i+1, j+1)
-                res += dfs(i+2, j+1)
-                res += dfs(i+1, j+2)
-            res += dfs(i, j+2)
-            return res % MOD
-        return dfs(0, 0)
+        def dfs(i, prev_gap):
+            if i > n: return 0
+            if i == n: return 1 if not prev_gap else 0
+            if prev_gap:
+                return (dfs(i+1, False) + dfs(i+1, True)) % MOD
+            else:
+                return (dfs(i+1, False) + dfs(i+2, False) + 2*dfs(i+2, True))%MOD
+        return dfs(0, False)
