4b8b90fb8aaadbcf5624057ecf9a635d99f44036
huytq <huytq000605@gmail.com>
2025-05-22 20:51:18 +0800

    Zero Arrays Transformation III

--- Priority Queue/Zero Array Transformation III/solution.cpp
+++ Priority Queue/Zero Array Transformation III/solution.cpp
@@ -4,21 +4,19 @@ public:
         sort(queries.begin(), queries.end());
         priority_queue<int> pq;
         int nq = queries.size();
-        vector<int> end(nums.size() + 1, 0);
-        for(int i{}, iq{}, nq = queries.size(), used{}; i < nums.size(); ++i) {
-            while(iq < nq && i >= queries[iq][0]) {
+        vector<int> end(nums.size(), 0);
+        int in_use = 0;
+        for(int i = 0, iq = 0; i < nums.size(); ++i) {
+            while(iq < nq && queries[iq][0] <= i) {
                 pq.emplace(queries[iq++][1]);
             }
-            
-            while(used < nums[i]) {
+            while(in_use < nums[i]) {
                 if(pq.empty() || pq.top() < i) return -1;
-                --end[pq.top()];
-                pq.pop();
-                ++used;
+                ++in_use;
+                --end[pq.top()]; pq.pop();
             }
-            used += end[i];
+            in_use += end[i];
         }
-        
         return pq.size();
     }
 };
