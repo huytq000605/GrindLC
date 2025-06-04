55744dac4f53eaf887c426147591115652f64df0
huytq <huytq000605@gmail.com>
2025-06-04 21:40:50 +0800

    Find the Lexicographically Largest String From the Box I

--- String/Find the Lexicographically Largest String From the Box I/solution.cpp
+++ String/Find the Lexicographically Largest String From the Box I/solution.cpp
@@ -3,22 +3,19 @@ public:
     string answerString(string word, int numFriends) {
         if(numFriends == 1) return word;
         int result{};
-        int target{static_cast<int>(word.size()) - numFriends + 1};
-        for(int i{1}, offset{}; i < word.size(); ++i) {
-            int d = word[result+offset] - word[i];
-            if(d == 0) {
-                ++offset;
-            } else if(d < 0) {
-                // dcbd
-                // dcbdce
-                // comparing the first character as the special case
-                if(word[result] < word[i]) result = i;
-                else result = i-offset;
-                offset = 0;
-            } else {
-                offset = 0;
+        int sz{static_cast<int>(word.size()) - numFriends + 1};
+        for(int i{1}; i < word.size(); ++i) {
+            bool lt = false;
+            for(int j = 0; j < sz && i + j < word.size(); ++j) {
+                if(word[result + j] > word[i + j]) break;
+                if(word[result + j] < word[i + j]) {
+                    lt = true;
+                }
+            }
+            if(lt) {
+                result = i;
             }
         }
-        return word.substr(result, target);
+        return word.substr(result, sz);
     }
 };
