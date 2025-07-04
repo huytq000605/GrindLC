aff083af89b5d276cad3473080a235b954a70247
Huy Tran <huytq000605@gmail.com>
2025-07-04 17:42:59 +0800

    Find the K-th character in String Game II

--- Math & Game Theory & Simulation/Find the K-th Character in String Game II/solution.cpp
+++ Math & Game Theory & Simulation/Find the K-th Character in String Game II/solution.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     char kthCharacter(long long k, vector<int>& operations) {
-        long long length = 1;
-        int i = -1;
-        while(length < k) {
-            length *= 2;
-            ++i;
-        }
-        int increased = 0;
-        while(length > 1) {
-            if(k > length / 2) {
-                increased += operations[i];
-                k -= length / 2;
+        long long t = ceil(log(k)/log(2));
+        long long n = 1ll << t;
+        long long shift = 0;
+        --k;
+        int i = t - 1;
+        while(n > 1) {
+            n >>= 1;
+            if(k >= n) {
+                k -= n;
+                shift = (shift + operations[i]) % 26;
             }
             --i;
-            length /= 2;
         }
-        return increased % 26 + 'a';
+        return 'a' + (shift % 26);
     }
 };
