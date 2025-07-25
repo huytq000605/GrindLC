3e06df64f0d84e78f85562904ca6278e209fe661
huytq <huytq000605@gmail.com>
2025-07-26 01:09:48 +0800

    Maximum Unique Subarray Sum Ater Deletion

--- Easy Guys/Maximum Unique Subarray Sum After Deletion/solution.cpp
+++ Easy Guys/Maximum Unique Subarray Sum After Deletion/solution.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int maxSum(vector<int>& nums) {
         int result = 0;
-        unordered_set<int> seen;
+        vector<int> seen(101);
+        bool no_positive = true;
         for(int i = 0; i < nums.size(); ++i) {
-            if(nums[i] > 0 && seen.find(nums[i]) == seen.end()) {
-                seen.emplace(nums[i]);
+            if(nums[i] > 0 && !seen[nums[i]]) {
+                seen[nums[i]] = 1;
                 result += nums[i];
+                no_positive = false;
             }
         }
-        if(seen.empty()) return *max_element(nums.begin(), nums.end());
+        if(no_positive) return *max_element(nums.begin(), nums.end());
         return result;
     }
 };
