685268d58c4dcf5cc8b47536ac1b0f469ebc2625
huytq <huytq000605@gmail.com>
2025-10-09 21:07:40 +0800

    Find the Minimum Amount of Time to Brew Potions

--- Math & Game Theory & Simulation/Find the Minimum Amount of Time to Brew Potions/solution.cpp
+++ Math & Game Theory & Simulation/Find the Minimum Amount of Time to Brew Potions/solution.cpp
@@ -3,12 +3,12 @@ public:
     long long minTime(vector<int>& skill, vector<int>& mana) {
         int n = skill.size(), m = mana.size();
         vector<long long> t(n);
-        for(int potion = 0; potion < m; ++potion) {
-            for(int p = n-2; p >= 0; --p) {
-                t[p] = max(t[p], t[p+1] - skill[p] * mana[potion]);
+        for(int p = 0; p < m; ++p) {
+            for(int w = n-2; w >= 0; --w) {
+                t[w] = max(t[w], t[w+1] - skill[w] * mana[p]);
             }
-            for(int p = 0; p < n; ++p) {
-                t[p] = max(p ? t[p-1]: 0, t[p]) + skill[p] * mana[potion];
+            for(int w = 0; w < n; ++w) {
+                t[w] = max(w ? t[w-1]: 0, t[w]) + skill[w] * mana[p];
             }
         }
         return t.back();
