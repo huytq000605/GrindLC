3f91647ef901e64fdb50a47b47f82e1c1c8db5b7
Huy Tran <huytq000605@gmail.com>
2025-10-28 23:44:10 +0700

    Make Array Elements Equal to Zero

--- Easy Guys/Make Array Elements Equal to Zero/solution.cpp
+++ Easy Guys/Make Array Elements Equal to Zero/solution.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
         int s = accumulate(nums.begin(), nums.end(), 0);
-        int prefix{};
-        int result{};
-        for(int num: nums) {
-            if(!num) {
-                if(prefix == s - prefix) result += 2;
-                else if(abs(s - 2 * prefix) == 1) result += 1;
+        int result = 0;
+        for(int i = 0, left = 0; i < nums.size(); ++i) {
+            left += nums[i];
+            if(!nums[i]) {
+                int d = abs(left - (s-left));
+                result += d == 0 ? 2: d == 1 ? 1: 0;
             }
-            
-            prefix += num;
         }
         return result;
     }
