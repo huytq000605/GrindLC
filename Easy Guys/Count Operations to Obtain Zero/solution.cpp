d26e265358b9f8f299f54c33f2b679a50c4856f7
huytq <huytq000605@gmail.com>
2025-11-09 15:32:53 +0800

    Count Operations to Obtain Zero

---
+++ Easy Guys/Count Operations to Obtain Zero/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int result = 0;
+        while(num1 != 0 && num2 != 0) {
+            if(num1 < num2) swap(num1, num2);
+            result += num1 / num2;
+            num1 %= num2;
+        }
+        return result;
+    }
+};
