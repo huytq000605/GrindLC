a6597ae17dd4c2aeccaec422a47387157574d239
huytq <huytq000605@gmail.com>
2025-11-15 17:29:11 +0800

    Count the Number of Substrings With Dominant Ones

--- Sliding Window/*Count the Number of Substrings With Dominant Ones/solution.cpp
+++ Sliding Window/*Count the Number of Substrings With Dominant Ones/solution.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        // a = number of ones
-        // b = number of zeros
-        // a + b = n
-        // b * b <= a <= n 
-        // b <= sqrt(n)
-        int sq = sqrt(s.size());
         int result = 0;
-        for(int b = 0; b <= sq; b++) {
-            int i = 0, j = 0;
-            int zeros = 0;
-            int ones_i = 0, ones_j = 0;
-            for(int idx = 0; idx < s.size(); idx++) {
-                char c = s[idx];
-                if(c == '0') zeros++;
-                else {
-                    ones_i++;
-                    ones_j++;
-                }
-                while(zeros > b) {
-                    if(s[i] == '0') zeros--;
-                    else ones_i--;
-                    i++;
-                }
-                while(j < i || 
-                (zeros == b && 
-                j + 1 <= idx && // Guarantee in case when b = 0, j can make ones_j to become 0
-                s[j] == '1' && ones_j > zeros * zeros)) {
-                    if(s[j] == '1') ones_j--;
-                    j++;
+        for(int z = 0; z + z*z <= s.size(); z++) {
+            vector<int> count(2);
+            int prev_result = result;
+            for(int i = 0, k = 0, j = 0; j < s.size(); ++j) {
+                count[s[j]-'0']++;
+                while(count[0] > z) count[s[i++]-'0']--;
+                if(count[0] == z && count[1] && count[1] >= z*z) {
+                    // move k to the last '1' we could start from to maintain z zeros
+                    for(k = max(k, i); k < j && s[k] == '1' && count[1] >= z*z; ++k);
+                    // we might move too far that we might not have enough ones
+                    // check that by min with count[1] - z*z
+                    result += 1 + min(k - i, count[1] - z*z);
+                    
                 }
                 
-                if(zeros == b && i <= idx && ones_j >= zeros * zeros) {
-                    result += j - i + 1;
-                }
             }
+            if(result == prev_result) break;
         }
         return result;
     }
