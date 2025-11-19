f222a1c35ced6fa937cdf00a33db708fe5e4c8b7
huytq <huytq000605@gmail.com>
2025-11-19 21:16:47 +0800

    Keep Multiplying Found Values by Two

---
+++ Easy Guys/Keep Multiplying Found Values by Two/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int num) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        while(s.find(num) != s.end()) {
+            num *= 2;
+        }
+        return num;
+    }
+};
