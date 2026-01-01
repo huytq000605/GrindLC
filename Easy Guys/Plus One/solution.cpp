cd9b81c34175358fc173c6a4af076402ae0d9fba
huytq <huytq000605@gmail.com>
2026-01-01 22:47:51 +0800

    Plus One

---
+++ Easy Guys/Plus One/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry = 1;
+        for(int i = digits.size() - 1; i >= 0; --i) {
+            digits[i] = digits[i] + carry;
+            carry = 0;
+            if(digits[i] == 10) {
+                digits[i] = 0;
+                carry = 1;
+            } else {
+                break;
+            }
+        }
+        if(carry) {
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+};
