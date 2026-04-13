78c1ad9fd1e1daa1cd138666292effb99aa3103c
huytq <huytq000605@gmail.com>
2026-04-14 01:09:20 +0800

    Minimum Distance to the Target Element

---
+++ Easy Guys/Minimum Distance to the Target Element/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        for(int i = 0; i < nums.size(); ++i) {
+            if(start - i >= 0 && nums[start-i] == target) return i;
+            if(start + i < nums.size() && nums[start+i] == target) return i;
+        }
+        return -1;
+    }
+};
