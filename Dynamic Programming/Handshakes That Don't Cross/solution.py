5a83b4960833e28056fbfbdeaf979fffd2bbb69a
huytq <huytq000605@gmail.com>
2026-06-16 02:42:22 +0800

    Handshakes That Don't Cross

--- Dynamic Programming/Handshakes That Don't Cross/solution.py
+++ Dynamic Programming/Handshakes That Don't Cross/solution.py
@@ -1,4 +1,17 @@
 class Solution:
+    def numberOfWays(self, n: int) -> int:
+        MOD = 10**9 + 7
+        # dp[i] = number of ways for i*2 people
+        dp = [0 for _ in range(n//2+1)]
+        dp[0] = 1
+        for i in range(1, n//2+1):
+            for j in range(i):
+                dp[i] = (dp[i] + dp[j] * dp[i-1-j]) % MOD
+        return dp[-1]
+        
+
+
+class Solution:
     def numberOfWays(self, numPeople: int) -> int:
         MOD = 10**9 + 7
         @cache
