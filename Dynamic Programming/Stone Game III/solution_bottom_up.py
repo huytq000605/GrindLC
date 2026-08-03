b02e3f1aa1a3b122f1db5d7ad60dd96145ff3bf1
huytq <huytq000605@gmail.com>
2026-08-03 20:12:28 +0800

    Stone Game III

--- Dynamic Programming/Stone Game III/solution_bottom_up.py
+++ Dynamic Programming/Stone Game III/solution_bottom_up.py
@@ -1,13 +1,15 @@
 class Solution:
     def stoneGameIII(self, piles: List[int]) -> str:
         n = len(piles)
-        dp = [-math.inf for _ in range(n+1)]
-        dp[-1] = 0
-        for i in reversed(range(n)):
+        dp = [-math.inf for _ in range(n)]
+        for i in range(n-1, -1, -1):
             s = 0
-            for j in range(i, min(n, i + 3)):
+            for j in range(i, min(i+3, n)):
                 s += piles[j]
-                dp[i] = max(dp[i], s - dp[j+1])
-        if dp[0] > 0: return "Alice"
-        elif dp[0] < 0: return "Bob"
-        return "Tie"
+                dp[i] = max(dp[i], s - (dp[j+1] if j+1 < n else 0))
+        if dp[0] > 0:
+            return "Alice"
+        elif dp[0] < 0:
+            return "Bob"
+        else:
+            return "Tie"
