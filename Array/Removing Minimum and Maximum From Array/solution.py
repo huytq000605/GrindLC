22f3c8d5552a26005d1509e2cc41117c0982680c
huytq <huytq000605@gmail.com>
2026-08-31 01:35:27 +0800

    Removing Minimum and Maximum From Array

--- Array/Removing Minimum and Maximum From Array/solution.py
+++ Array/Removing Minimum and Maximum From Array/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def minimumDeletions(self, nums: List[int]) -> int:
-        minIdx = 0
-        maxIdx = 0
-        for i, num in enumerate(nums):
-            if num < nums[minIdx]:
-                minIdx = i
-            if num > nums[maxIdx]:
-                maxIdx = i
+        mn, mx = 0, 0
         n = len(nums)
-        result = min(max(maxIdx + 1, minIdx + 1), max(n - maxIdx, n - minIdx), minIdx + 1 + (n - maxIdx), maxIdx + 1 + (n- minIdx))
-        return result
+        for i in range(n):
+            if nums[i] < nums[mn]:
+                mn = i
+            if nums[i] > nums[mx]:
+                mx = i
+        i = min(mn, mx)
+        j = max(mn, mx)
+        return min(j+1, n-i, i+1 + n-j)
