0ca580d6a0381198924adb14712ae07782f3ce88
huytq <huytq000605@gmail.com>
2026-09-07 02:47:06 +0800

    Distinct Subsequences

--- Dynamic Programming/Distinct Subsequences/solution.py
+++ Dynamic Programming/Distinct Subsequences/solution.py
@@ -1,5 +1,17 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
+        ns, nt = len(s), len(t)
+        dp = [0 for _ in range(nt + 1)]
+        dp[0] = 1
+        for i in range(ns):
+            for j in range(nt-1, -1, -1):
+                if s[i] == t[j]:
+                    dp[j+1] += dp[j]
+        return dp[-1] 
+
+"""
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
         @cache
         def dfs(idx1, idx2):
             if idx2 >= len(t):
@@ -12,3 +24,4 @@ class Solution:
             result += dfs(idx1 + 1, idx2)
             return result
         return dfs(0, 0)
+"""
