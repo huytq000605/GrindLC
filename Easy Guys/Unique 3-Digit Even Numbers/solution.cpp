4c4b62875309fabe32e14ae8eacb636d4bd25021
huytq <huytq000605@gmail.com>
2026-09-11 22:08:17 +0800

    Unique 3-Digit Even Numbers

--- Easy Guys/Unique 3-Digit Even Numbers/solution.cpp
+++ Easy Guys/Unique 3-Digit Even Numbers/solution.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        int n = digits.size();
-        unordered_set<int> seen;
-        for(int i{}; i < n; ++i) {
-            if(digits[i] == 0) continue;
-            for(int j{}; j < n; ++j) {
-                if(i == j) continue;
-                for(int k{}; k < n; ++k) {
-                    if(i == k || j == k || digits[k] & 1) continue;
-                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
-                    seen.emplace(num);
+        int result = 0;
+        vector<int> counter(10);
+        for(int d: digits) counter[d]++;
+        for(int d3 = 0; d3 < 10; d3 += 2) {
+            if(!counter[d3]) continue;
+            counter[d3]--;
+            for(int d2 = 0; d2 < 10; ++d2) {
+                if(!counter[d2]) continue;
+                counter[d2]--;
+                for(int d1 = 1; d1 < 10; ++d1) {
+                    if(!counter[d1]) continue;
+                    result += 1;
                 }
+                counter[d2]++;
             }
+            counter[d3]++;
         }
-        return seen.size();
+        return result;
     }
 };
