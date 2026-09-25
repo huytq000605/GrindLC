b71cdf0aa7357a8636bf48968c546c8517dbb6be
huytq <huytq000605@gmail.com>
2026-09-26 04:28:33 +0800

    Brace Expansion II

--- Stack & Queue/*Brace Expansion II/solution.py
+++ Stack & Queue/*Brace Expansion II/solution.py
@@ -1,27 +1,42 @@
 class Solution:
-    def braceExpansionII(self, expression: str) -> List[str]:
-        result = set()
-        def dfs(idx, current, stack):
-            nonlocal result
-            for i in range(idx, len(current)):
-                if current[i] == "{":
-                    stack.append(i)
-                elif current[i] == "}":
-                    start = stack.pop()
-                    selections = current[start + 1: i].split(",")
-                    for selection in selections:
-                        newCurrent = current[0:start] + selection + current[i+1:]
-                        dfs(start + len(selection), newCurrent, [*stack])
-                    return
-            result.add(current)
+    def braceExpansionII(self, expr: str) -> list[str]:
+        n = len(expr)
+        stack = []
+        ops = []
+        def ope():
+            r, l = stack.pop(), stack.pop()
+            result = set()
+            op = ops.pop()
+            if op == "*":
+                for ll in l:
+                    for rr in r:
+                        result.add(ll + rr)
+            elif op == "+":
+                result = l | r
+            stack.append(result)
+            return result  
         
-        dfs(0, expression, [])
+        for i, ch in enumerate(expr):
+            if ch == ',':
+                while ops and ops[-1] == "*":
+                    ope()
+                ops.append("+")
+            elif ch == '{':
+                if i > 0 and (expr[i-1] == '}' or expr[i-1].isalpha()):
+                    ops.append("*")
+                ops.append('{')
+            elif ch == '}':
+                while ops[-1] != '{':
+                    ope()
+                ops.pop()
+            else:
+                if i > 0 and (expr[i-1] == '}' or expr[i-1].isalpha()):
+                    ops.append("*")
+                stack.append({ch})
         
-        finalResult = set()
-        for res in result:
-            res = res.split(",")
-            for r in res: finalResult.add(r)
-            
-        finalResult = sorted(list(finalResult))
-        
-        return finalResult
+        while ops:
+            ope()
+        return sorted(stack[-1])
+
+                
+
